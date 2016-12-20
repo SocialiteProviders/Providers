320cb3e4e4d7820e7c1e4399d5e71fc853fba730
Pavan Kumar <katakampavan.btech@gmail.com>
2016-12-20 09:21:09 +0530

    style ci fixes

--- src/Acclaim/Provider.php
+++ src/Acclaim/Provider.php
@@ -2,9 +2,10 @@
 
 namespace SocialiteProviders\Acclaim;
 
+use SocialiteProviders\Manager\OAuth2\User;
 use Laravel\Socialite\Two\ProviderInterface;
 use SocialiteProviders\Manager\OAuth2\AbstractProvider;
-use SocialiteProviders\Manager\OAuth2\User;
+
 
 class Provider extends AbstractProvider implements ProviderInterface
 {
@@ -68,7 +69,7 @@ class Provider extends AbstractProvider implements ProviderInterface
     protected function getTokenFields($code)
     {
         return array_merge(parent::getTokenFields($code), [
-            'grant_type' => 'authorization_code'
+            'grant_type' => 'authorization_code',
         ]);
     }
 }
