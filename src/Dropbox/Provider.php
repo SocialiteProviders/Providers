241d170f3ba85c8c6bbb4f67bc06cbd6d2a08b96
iemand002 <iemand002@users.noreply.github.com>
2017-01-17 10:23:24 +0100

    Check if no dropbox avatar exists
    Forgot to check what it does if there is no avatar => $user['profile_photo_url'] does not exist so check if isset

--- src/Dropbox/Provider.php
+++ src/Dropbox/Provider.php
@@ -56,7 +56,7 @@ class Provider extends AbstractProvider implements ProviderInterface
             'nickname' => null,
             'name' => $user['name']['display_name'],
             'email' => $user['email'],
-            'avatar' => $user['profile_photo_url'],
+            'avatar' => isset($user['profile_photo_url']) ? $user['profile_photo_url'] : null,
         ]);
     }
 
