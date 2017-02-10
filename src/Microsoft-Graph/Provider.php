8b1e15ef96364cb082a7a2bc78f15c54c2608549
Brian Faust <faustbrian@users.noreply.github.com>
2017-02-10 10:39:18 +0200

    Apply fixes from StyleCI (#32)
    [ci skip] [skip ci]

--- src/Microsoft-Graph/Provider.php
+++ src/Microsoft-Graph/Provider.php
@@ -24,7 +24,7 @@ class Provider extends AbstractProvider implements ProviderInterface
      * @var string
      */
     protected $scopeSeparator = ' ';
-    
+
     /**
      * {@inheritdoc}
      */
