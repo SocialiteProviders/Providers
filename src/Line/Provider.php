fe46e0bc1506a20fb4ee4d2fe1b168b18786b91f
mix5003 <mix5003@gmail.com>
2017-02-20 23:52:33 +0700

    Add Line Provider

---
+++ src/Line/Provider.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace SocialiteProviders\Line;
+
+use SocialiteProviders\Manager\OAuth2\User;
+use Laravel\Socialite\Two\ProviderInterface;
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+
+class Provider extends AbstractProvider implements ProviderInterface
+{
+
+    /**
+     * Unique Provider Identifier.
+     */
+    const IDENTIFIER = 'LINE';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase(
+            'https://access.line.me/dialog/oauth/weblogin', $state
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://api.line.me/v2/oauth/accessToken';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get(
+            'https://api.line.me/v2/profile', [
+            'headers' => [
+                'Authorization' => 'Bearer '.$token,
+            ],
+        ]);
+
+        return json_decode($response->getBody()->getContents(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id' => $user['userId'],
+            'nickname' => null,
+            'name' => $user['displayName'],
+            'email' => null,
+            'avatar' => $user['pictureUrl'],
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code'
+        ]);
+    }
+}
