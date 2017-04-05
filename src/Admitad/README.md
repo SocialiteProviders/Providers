319a1769327e266e8b22ccf40a88983f0bf7e1ac
boston <boston@boston-mbp.local>
2017-04-05 18:31:47 +0300

    Added Admitad provider

---
+++ src/Admitad/README.md
@@ -0,0 +1,11 @@
+# Admitad OAuth2 Provider for Laravel Socialite
+
+[![Scrutinizer Code Quality](https://img.shields.io/scrutinizer/g/SocialiteProviders/Admitad.svg?style=flat-square)](https://scrutinizer-ci.com/g/SocialiteProviders/Admitad/?branch=master)
+[![Latest Stable Version](https://img.shields.io/packagist/v/socialiteproviders/admitad.svg?style=flat-square)](https://packagist.org/packages/socialiteproviders/admitad)
+[![Total Downloads](https://img.shields.io/packagist/dt/socialiteproviders/admitad.svg?style=flat-square)](https://packagist.org/packages/socialiteproviders/admitad)
+[![Latest Unstable Version](https://img.shields.io/packagist/vpre/socialiteproviders/admitad.svg?style=flat-square)](https://packagist.org/packages/socialiteproviders/admitad)
+[![License](https://img.shields.io/packagist/l/socialiteproviders/admitad.svg?style=flat-square)](https://packagist.org/packages/socialiteproviders/admitad)
+
+## Documentation
+
+Full documentation for using this provider can be found at [Admitad Documentation](http://socialiteproviders.github.io/providers/admitad/)
---
+++ src/Admitad/src/Provider.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace SocialiteProviders\Admitad;
+
+use Laravel\Socialite\Two\ProviderInterface;
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider implements ProviderInterface
+{
+    /**
+     * Unique Provider Identifier.
+     */
+    const IDENTIFIER = 'ADMITAD';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopes = ['private_data private_data_email'];
+
+    protected $host = 'https://api.admitad.com';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase($this->host.'/authorize/', $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return $this->host.'/token/';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get($this->host.'/me/', [
+            'headers' => [
+                'Authorization' => 'Bearer '.$token,
+            ],
+        ])
+        ;
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id'       => $user['id'],
+            'nickname' => $user['username'],
+            'name'     => sprintf('%s %s', $user['first_name'], $user['last_name']),
+            'email'    => $user['email'],
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code',
+        ]);
+    }
+}
