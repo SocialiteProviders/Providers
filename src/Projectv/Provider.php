f384ee3aa2f43c29e0a2534fda030f1c350fde55
vreezy117 <vreezy117@googlemail.com>
2017-06-02 11:38:28 +0200

    seriously removed 2 tabs because of StyleCI

--- src/Projectv/Provider.php
+++ src/Projectv/Provider.php
@@ -56,7 +56,7 @@ class Provider extends AbstractProvider implements ProviderInterface
                 'Authorization' => 'Bearer '.$token,
             ],
         ]);
-        
+
         return json_decode($response->getBody()->getContents(), true);
     }
 
