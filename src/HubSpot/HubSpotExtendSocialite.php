d606fbc54140a87302d4f000d1c0def1c9a2f3e7
Steve Williamson <steve@sjdub.com>
2017-06-13 13:27:04 -0400

    HubSpot provider

---
+++ src/HubSpot/HubSpotExtendSocialite.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace SocialiteProviders\HubSpot;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class HubSpotExtendSocialite
+{
+    /**
+     * Register the provider.
+     *
+     * @param \SocialiteProviders\Manager\SocialiteWasCalled $socialiteWasCalled
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite(
+            'hubspot', __NAMESPACE__.'\Provider'
+        );
+    }
+}
