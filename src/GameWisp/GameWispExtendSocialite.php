602f08733f578ebbb33c19e994b288a5b6610e31
Eli Hooten <elir.hooten@gmail.com>
2017-06-16 08:19:58 -0500

    GameWisp Provider (#83)
    * GameWisp Provider
    
    Added support for GameWisp. A fan funding service for livestreamers
    
    * Fix Typo in split.sh
    
    * Space error in split.sh

---
+++ src/GameWisp/GameWispExtendSocialite.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace SocialiteProviders\GameWisp;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class GameWispExtendSocialite
+{
+    /**
+     * Register the provider.
+     *
+     * @param \SocialiteProviders\Manager\SocialiteWasCalled $socialiteWasCalled
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite(
+            'gamewisp', __NAMESPACE__.'\Provider'
+        );
+    }
+}
