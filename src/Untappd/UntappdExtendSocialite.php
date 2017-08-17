bd39512e3bb9b01e0a32fc74bb2bdfb6db2b6b1d
Stas <stanislav.arsoba@gmail.com>
2017-08-17 20:43:08 +0300

    Added Untappd OAuth2 Provider

---
+++ src/Untappd/UntappdExtendSocialite.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace SocialiteProviders\Untappd;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class UntappdExtendSocialite
+{
+    /**
+     * Register the provider.
+     *
+     * @param \SocialiteProviders\Manager\SocialiteWasCalled $socialiteWasCalled
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite(
+            'untappd', __NAMESPACE__ . '\Provider'
+        );
+    }
+}
