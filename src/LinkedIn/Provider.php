a5eddbababf6cbf4c8b306d4a9b7fff68437dad5
_Axieum <Axieum@users.noreply.github.com>
2017-12-03 02:03:17 +1000

    Added 'public-profile-url' to LinkedIn request for convenience (#120)

--- src/LinkedIn/Provider.php
+++ src/LinkedIn/Provider.php
@@ -94,7 +94,7 @@ class Provider extends AbstractProvider implements ProviderInterface
     protected function getUserByToken($token)
     {
         $response = $this->getHttpClient()->get(
-            'https://api.linkedin.com/v1/people/~:(id,formatted-name,picture-url,email-address)', [
+            'https://api.linkedin.com/v1/people/~:(id,formatted-name,picture-url,email-address,public-profile-url)', [
             'headers' => [
                 'Accept-Language' => 'en-US',
                 'x-li-format' => 'json',
