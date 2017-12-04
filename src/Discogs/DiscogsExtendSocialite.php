d82ad82d5bbde42dbd4a6969144c301d5c52a61a
Blake Bengtson <blake@bengtson.us>
2017-12-03 23:09:52 -0600

    Added Discogs provider (#122)
    * Added Discogs provider
    
    * Added discogs to split.sh. Added php 7 and managers 3 to composer.json

---
+++ src/Discogs/DiscogsExtendSocialite.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace SocialiteProviders\Discogs;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class DiscogsExtendSocialite
+{
+    /**
+     * Execute the provider.
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite(
+            'discogs',
+            __NAMESPACE__.'\Provider',
+            __NAMESPACE__.'\Server'
+        );
+    }
+}
