b46cf2c07c42812f9cb91c70e737079e343dccff
jim <jim@blueberry.nl>
2017-12-18 02:36:11 +0100

    added new Microsoft provider

---
+++ src/Microsoft/Provider.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace SocialiteProviders\Microsoft;
+
+use SocialiteProviders\Manager\OAuth2\User;
+use Laravel\Socialite\Two\ProviderInterface;
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+
+class Provider extends AbstractProvider implements ProviderInterface
+{
+    /**
+     * Unique Provider Identifier.
+     */
+    const IDENTIFIER = 'MICROSOFT';
+
+    /**
+     * {@inheritdoc}
+     * https://msdn.microsoft.com/en-us/library/azure/ad/graph/howto/azure-ad-graph-api-permission-scopes.
+     */
+    protected $scopes = ['User.Read'];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopeSeparator = ' ';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return
+            $this->buildAuthUrlFromBase(
+                'https://login.microsoftonline.com/'.($this->config['tenant'] ?: 'common').'/oauth2/v2.0/authorize',
+                $state
+            );
+    }
+
+    /**
+     * {@inheritdoc}
+     * https://developer.microsoft.com/en-us/graph/docs/concepts/use_the_api.
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://login.microsoftonline.com/common/oauth2/v2.0/token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get(
+            'https://graph.microsoft.com/v1.0/me',
+            [
+                'headers' => [
+                    'Accept' => 'application/json',
+                    'Authorization' => 'Bearer '.$token,
+                ],
+            ]
+        );
+
+        return json_decode($response->getBody()->getContents(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id' => $user['id'],
+            'nickname' => null,
+            'name' => $user['displayName'],
+            'email' => $user['mail'],
+            'avatar' => null,
+
+            'businessPhones' => $user['businessPhones'],
+            'displayName' => $user['displayName'],
+            'givenName' => $user['givenName'],
+            'jobTitle' => $user['jobTitle'],
+            'mail' => $user['mail'],
+            'mobilePhone' => $user['mobilePhone'],
+            'officeLocation' => $user['officeLocation'],
+            'preferredLanguage' => $user['preferredLanguage'],
+            'surname' => $user['surname'],
+            'userPrincipalName' => $user['userPrincipalName'],
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code',
+        ]);
+    }
+
+    /**
+     * Add the additional configuration key 'tenant' to enable the branded sign-in experience.
+     *
+     * @return array
+     */
+    public static function additionalConfigKeys()
+    {
+        return ['tenant'];
+    }
+}
