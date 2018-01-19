0bdf585fa811db78115cdd223439edec77df63c4
Recca Tsai <recca0120@gmail.com>
2018-01-19 00:58:59 -0600

    add Laravel Passport Provider (#133)
    * laravel passport provider
    
    * update
    
    * Delete .editorconfig
    
    * remove
    
    * move
    
    * change path

---
+++ src/LaravelPassport/Provider.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace SocialiteProviders\LaravelPassport;
+
+use Illuminate\Support\Arr;
+use SocialiteProviders\Manager\OAuth2\User;
+use Laravel\Socialite\Two\ProviderInterface;
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+
+class Provider extends AbstractProvider implements ProviderInterface
+{
+    /**
+     * Unique Provider Identifier.
+     */
+    const IDENTIFIER = 'LARAVELPASSPORT';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopes = [''];
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function additionalConfigKeys()
+    {
+        return ['host', 'authorize_uri', 'token_uri', 'userinfo_uri', 'userinfo_key'];
+    }
+
+    /**
+     * Get the authentication URL for the provider.
+     *
+     * @param string $state
+     *
+     * @return string
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase($this->getLaravelPassportUrl('authorize_uri'), $state);
+    }
+
+    /**
+     * Get the token URL for the provider.
+     *
+     * @return string
+     */
+    protected function getTokenUrl()
+    {
+        return $this->getLaravelPassportUrl('token_uri');
+    }
+
+    /**
+     * Get the raw user for the given access token.
+     *
+     * @param string $token
+     *
+     * @return array
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get($this->getLaravelPassportUrl('userinfo_uri'), [
+            'headers' => [
+                'Authorization' => 'Bearer '.$token,
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * Map the raw user array to a Socialite User instance.
+     *
+     * @param array $user
+     *
+     * @return \Laravel\Socialite\User
+     */
+    protected function mapUserToObject(array $user)
+    {
+        $key = $this->getConfig('userinfo_key', null);
+        $data = is_null($key) === true ? $user : Arr::get($user, $key, []);
+
+        return (new User())->setRaw($data)->map([
+            'id' => Arr::get($data, 'id'),
+            'nickname' => Arr::get($data, 'username'),
+            'name' => Arr::get($data, 'name'),
+            'email' => Arr::get($data, 'email'),
+            'avatar' => Arr::get($user, 'avatar'),
+        ]);
+    }
+
+    /**
+     * Get the POST fields for the token request.
+     *
+     * @param string $code
+     *
+     * @return array
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code',
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getLaravelPassportUrl($type)
+    {
+        return rtrim($this->getConfig('host'), '/').'/'.ltrim(($this->getConfig($type, Arr::get([
+            'authorize_uri' => 'oauth/authorize',
+            'token_uri' => 'oauth/token',
+            'userinfo_uri' => 'api/user',
+        ], $type))), '/');
+    }
+}
