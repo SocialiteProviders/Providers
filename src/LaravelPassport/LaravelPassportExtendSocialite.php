151355203ae03c5fb278888c42ef48074bf98d2a
recca0120 <recca0120@gmail.com>
2018-01-19 13:03:49 +0800

    move

---
+++ src/LaravelPassport/LaravelPassportExtendSocialite.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SocialiteProviders\LaravelPassport;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class LaravelPassportExtendSocialite
+{
+    /**
+     * Execute the provider.
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite('laravelpassport', __NAMESPACE__.'\Provider');
+    }
+}
