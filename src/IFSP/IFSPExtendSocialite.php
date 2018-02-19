fc1e62351b949fe21d102054751e707d7ce2b9ab
Douglas Andrade <douglasanpa@gmail.com>
2018-02-18 22:17:29 -0300

    Add IFSP OAUTH

---
+++ src/IFSP/IFSPExtendSocialite.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SocialiteProviders\IFSP;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class IFSPExtendSocialite
+{
+    /**
+     * Execute the provider.
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite('ifsp', __NAMESPACE__.'\Provider');
+    }
+}
