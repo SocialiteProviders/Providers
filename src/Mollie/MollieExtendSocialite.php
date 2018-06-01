3bf77437ca903ce9fd52b90ca867d64dbd9f24ea
Nick <nickurt@users.noreply.github.com>
2018-06-01 09:35:54 +0200

    Add Mollie Payment Provider (#178)
    * added support for mollie oauth2 ...
    
    * fixed and cleanup provider-file ..
    
    * applied fixes from styleci ..

---
+++ src/Mollie/MollieExtendSocialite.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace SocialiteProviders\Mollie;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class MollieExtendSocialite
+{
+    /**
+     * Register the provider.
+     *
+     * @param \SocialiteProviders\Manager\SocialiteWasCalled $socialiteWasCalled
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite(
+            'mollie', __NAMESPACE__.'\Provider'
+        );
+    }
+}
---
+++ src/Mollie/Provider.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace SocialiteProviders\Mollie;
+
+use Laravel\Socialite\Two\ProviderInterface;
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider implements ProviderInterface
+{
+    /**
+     * Unique Provider Identifier.
+     */
+    const IDENTIFIER = 'MOLLIE';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopes = ['organizations.read'];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopeSeparator = ' ';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase(
+            'https://www.mollie.com/oauth2/authorize', $state
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://api.mollie.nl/oauth2/tokens';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAccessToken($code)
+    {
+        $response = $this->getHttpClient()->post($this->getTokenUrl(), [
+            'headers'     => ['Authorization' => 'Basic '.base64_encode($this->clientId.':'.$this->clientSecret)],
+            'form_params' => $this->getTokenFields($code),
+        ]);
+
+        $this->credentialsResponseBody = json_decode($response->getBody(), true);
+
+        return $this->parseAccessToken($response->getBody());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRefreshTokenResponse($refreshToken)
+    {
+        $response = $this->getHttpClient()->post($this->getTokenUrl(), [
+            'headers'     => ['Accept' => 'application/json'],
+            'form_params' => $this->getRefreshTokenFields($refreshToken),
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get('https://api.mollie.nl/v1/organizations/me', [
+            'headers' => ['Authorization' => 'Bearer '.$token],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRefreshTokenFields($refreshToken)
+    {
+        return [
+            'client_id'     => $this->clientId,
+            'client_secret' => $this->clientSecret,
+            'grant_type'    => 'refresh_token',
+            'refresh_token' => $refreshToken,
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id'       => $user['id'],
+            'nickname' => $user['name'],
+            'name'     => $user['name'],
+            'email'    => $user['email'],
+            'avatar'   => null,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code',
+        ]);
+    }
+}
