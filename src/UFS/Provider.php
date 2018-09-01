686f5c9c2fc42178b2a6cad5ff6d05b93acd00f3
Claudson Martins <claudson@outlook.com>
2018-09-01 15:08:22 -0300

    Added provider for Universidade Federal de Sergipe

---
+++ src/UFS/Provider.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace SocialiteProviders\UFS;
+
+use Laravel\Socialite\Two\ProviderInterface;
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider implements ProviderInterface
+{
+    /**
+     * Unique Provider Identifier.
+     */
+    const IDENTIFIER = 'UFS';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopes = [''];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase('https://apisistemas.desenvolvimento.ufs.br/api/rest/authorization', $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://apisistemas.desenvolvimento.ufs.br/api/rest/token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get('https://apisistemas.desenvolvimento.ufs.br/api/rest/usuario', [
+            'headers' => [
+                'Authorization' => 'Bearer '.$token,
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id'       => null,
+            'nickname' => null,
+            'name'     => $user['pessoa']['nome'],
+            'email'    => $user['pessoa']['email'],
+            'avatar'   => null,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code'
+        ]);
+    }
+}
