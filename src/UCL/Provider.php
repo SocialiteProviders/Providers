1a54182d558345a5f7974fca49e7f3f2b747903c
Balázs Kovács <balping314@gmail.com>
2018-09-20 00:26:33 +0200

    UCL API provider
    https://uclapi.com/docs/#oauth

---
+++ src/UCL/Provider.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace SocialiteProviders\UCL;
+
+use Illuminate\Support\Arr;
+use Laravel\Socialite\Two\InvalidStateException;
+use Laravel\Socialite\Two\ProviderInterface;
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider implements ProviderInterface
+{
+    /**
+     * Unique Provider Identifier.
+     */
+    const IDENTIFIER = 'UCL';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase('https://uclapi.com/oauth/authorise/', $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://uclapi.com/oauth/token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function user()
+    {
+        if ($this->hasInvalidState() || !$this->getCode()) {
+            throw new InvalidStateException;
+        }
+        $response = $this->getAccessTokenResponse($this->getCode());
+        $user = $this->mapUserToObject($this->getUserByToken(
+            $token = Arr::get($response, 'token')
+        ));
+        return $user->setToken($token);
+    }
+
+    /**
+     * Get the POST fields for the token request.
+     *
+     * @param  string  $code
+     * @return array
+     */
+    protected function getTokenFields($code)
+    {
+        return [
+            'client_id' => $this->clientId,
+            'client_secret' => $this->clientSecret,
+            'code' => $code,
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get('https://uclapi.com/oauth/user/data', ['query' => [
+            "client_secret" => $this->clientSecret,
+            "token"         => $token,
+        ]]);
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User)->setRaw($user)->map([
+            'id' => Arr::get($user, 'upi'),
+            'name' => Arr::get($user, 'full_name'),
+            'nickname' => Arr::get($user, 'given_name'),
+            'email' => Arr::get($user, 'email'),
+        ]);
+    }
+}
---
+++ src/UCL/UCLExtendSocialite.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SocialiteProviders\UCL;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class UCLExtendSocialite
+{
+    /**
+     * Execute the provider.
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite('uclapi', __NAMESPACE__.'\Provider');
+    }
+}
