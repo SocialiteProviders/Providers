39cbf291612ef144ba427a41e540fe4295f3a27b
Ben Cornwell <ben@bencornwell.com>
2018-09-25 14:00:15 +1000

    initial commit - Socialite oAuth Provider for ORCID

---
+++ src/Orcid/OrcidExtendSocialite.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace SocialiteProviders\Orcid;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class OrcidExtendSocialite
+{
+    /**
+     * Register the provider.
+     *
+     * @param \SocialiteProviders\Manager\SocialiteWasCalled $socialiteWasCalled
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite(
+            'orcid', __NAMESPACE__.'\Provider'
+        );
+    }
+}
---
+++ src/Orcid/Provider.php
@@ -0,0 +1,219 @@
+<?php
+
+namespace SocialiteProviders\Orcid;
+
+use Exception;
+use Illuminate\Support\Arr;
+use Laravel\Socialite\Two\AbstractProvider;
+use Laravel\Socialite\Two\ProviderInterface;
+use Laravel\Socialite\Two\User;
+
+class OrcidProvider extends AbstractProvider implements ProviderInterface
+{
+    /**
+     * Unique Provider Identifier
+     */
+    const IDENTIFIER = 'ORCID';
+
+    /**
+     * Base URL for ORCID Sandpit Environment
+     */
+	const sandboxURL = 'https://sandbox.orcid.org/';
+
+    /**
+     * Base URL for ORCID Production Environment 
+     */
+	const productionURL = 'https://orcid.org/';
+
+    /**
+     * Profile Data URL for ORCID Sandpit Environment 
+     */
+	const sandboxProfileURL = 'https://pub.sandbox.orcid.org/v2.1/';
+
+    /**
+     * Profile Data URL for ORCID Production Environment 
+     */
+	const productionProfileURL = 'https://pub.orcid.org/v2.1/';
+
+    /**
+     * The scopes being requested.  
+     * Others include: '/activities/update','/person/update'
+     *
+     * You can customise the scopes when invoking the ORCID Socialite provider
+     * if this needs to change
+     *
+     * @var array
+     */
+    protected $scopes = ['/authenticate','/read-limited' ];
+
+    /**
+     * The separating character for the requested scopes.
+     *
+     * @var string
+     */
+    protected $scopeSeparator = ' ';
+
+    /**
+     * Tests whether we are integrating to the ORCID Sandbox or Production environments
+     * Change the value of ORCID_ENVIRONMENT in your .env to switch.
+     *
+     * @return boolean
+     */
+    protected function useSandbox( )
+    {
+    	return ( env('ORCID_ENVIRONMENT' ) != "production" );
+    }
+
+    /**
+     * Concatenate a base URL for ORCID oAuth requests 
+     *
+     * @return string
+     */
+    protected function baseUrl( $path )
+    {
+    	return ( $this->useSandbox()  ? OrcidProvider::sandboxURL : OrcidProvider::productionURL ) . $path;
+    }
+
+    /**
+     * Concatenate a base URL for ORCID profile data requests 
+     *
+     * @return string
+     */
+    protected function profileUrl( $path )
+    {
+    	return ( $this->useSandbox()  ? OrcidProvider::sandboxProfileURL : OrcidProvider::productionProfileURL ) . $path;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase( $this->baseUrl( 'oauth/authorize' ), $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return $this->baseUrl( 'oauth/token' );
+    }
+
+    /**
+     * Assuming authentication and token generation succeeeds, return a User object
+     *
+     * @return $user Laravel\Socialite\Two\User
+     */
+    public function user()
+    {
+        if ($this->hasInvalidState()) {
+            throw new InvalidStateException;
+        }
+ 
+        $response = $this->getAccessTokenResponse($this->getCode());
+ 
+        $user = $this->mapUserToObject($this->getUserByToken(
+            $response
+        ));
+
+        $token = Arr::get($response, 'access_token');
+ 
+        return $user->setToken($token)
+                    ->setRefreshToken(Arr::get($response, 'refresh_token'))
+                    ->setExpiresIn(Arr::get($response, 'expires_in'));
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+
+        $orcid = Arr::get($token, 'orcid');
+        $token = Arr::get($token, 'access_token');
+
+        $userUrl = $this->profileUrl( "{$orcid}/record" );
+        $response = $this->getHttpClient()
+                    ->get(
+                        $userUrl,
+                        [ 'headers' => 
+                           ['Content-Type' => 'application/vnd.orcid+xml', 
+                            'Accept' => 'application/json',
+                            'Authorization type' => 'Bearer',
+                            'Access token' => $token ],
+                        ]
+                    );
+
+        $user = json_decode($response->getBody(), true);
+
+        $user["email"] = $this->getEmail($user);
+        
+
+        return $user;
+    }
+
+    /**
+     * Get the email for the given access token.
+     *
+     * NOTE: this doesn't alway succeed becuase ORCID gives users the option to keep their email private
+     * If your app design relies on fetching the user email from ORCID, you should consider checking
+     * that it exists in your LoginController logic. 
+     *
+     * @param  string  $token
+     * @return string|null
+     */
+    protected function getEmail($user)
+    {
+        foreach( $user["person"]["emails"]["email"] as $m ){
+            if( $m["primary"] == true AND $m["verified"] == true )
+            {
+                return $m["email"];
+            }
+        }        	
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User)->setRaw($user)->map([
+            env( 'ORCID_UID_FIELDNAME', 'id' ) => $user['orcid-identifier']['path'], 
+            'nickname' => $user["person"]["name"]["given-names"]["value"],
+            'name' => sprintf( "%s %s", $user["person"]["name"]["given-names"]["value"], $user["person"]["name"]["family-name"]["value"] ),
+            'email' => array_get($user, 'email'),
+        ]);
+    }
+
+    /**
+     * Get the access token for the given code.
+     *
+     * @param  string  $code
+     * @return string
+     */
+    public function getAccessToken($code)
+    {
+        $s = $this->scopes[0];
+        $data = "client_id={$this->clientId}&client_secret={$this->clientSecret}&grant_type=client_credentials&scope={$s}";
+
+        $response = $this->getHttpClient()->post($this->getTokenUrl(), [
+            'headers' => ['Accept' => 'application/json','Content-Type' => 'application/x-www-form-urlencoded',],
+            'body' => $data,
+        ]);
+
+        return json_decode($response->getBody(), true)['access_token'];
+    }
+
+    /**
+     * Get the POST fields for the token request.
+     *
+     * @param  string  $code
+     * @return array
+     */
+    protected function getTokenFields($code)
+    {
+        return parent::getTokenFields($code) + ['grant_type' => 'authorization_code', 'orcid' => 'orcid'];
+    }
+}
