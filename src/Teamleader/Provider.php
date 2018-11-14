339d2380c07952df749efa13498f36ce943d29f3
Martin Skroch <martin.sk@13-grad.com>
2018-11-14 12:18:12 +0100

    Added Teamleader socialite provider

---
+++ src/Teamleader/Provider.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace SocialiteProviders\Teamleader;
+
+use Laravel\Socialite\Two\ProviderInterface;
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider implements ProviderInterface
+{
+    /**
+     * Unique Provider Identifier.
+     */
+    const IDENTIFIER = 'TEAMLEADER';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopes = ['users'];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase('https://app.teamleader.eu/oauth2/authorize', $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://app.teamleader.eu/oauth2/access_token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get('https://api.teamleader.eu/users.me', [
+            'headers' => [
+                'Authorization' => 'Bearer '.$token,
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id'       => $user['data']['id'],
+            'nickname' => $user['data']['first_name'],
+            'name'     => $user['data']['first_name'] . ' ' . $user['data']['last_name'],
+            'email'    => $user['data']['email'],
+            'avatar'   => null,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code'
+        ]);
+    }
+}
