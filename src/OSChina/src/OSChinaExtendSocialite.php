7bf08241f1a041d20e5d91e81838e8f0ac65f327
dingdayu <614422099@qq.com>
2019-01-07 16:54:19 +0800

    add `socialiteproviders/oschina`
    Signed-off-by: dingdayu <614422099@qq.com>

---
+++ src/OSChina/src/OSChinaExtendSocialite.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SocialiteProviders\OSChina;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class OSChinaExtendSocialite
+{
+    /**
+     * Execute the provider.
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite('oschina', __NAMESPACE__.'\Provider');
+    }
+}
