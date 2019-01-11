4128dff8f1ee6050556e854aada64a3f29c3f0a2
a-komarev <ell@cybercog.su>
2019-01-11 22:19:20 +0300

    Providers from stock should extend manager interfaces

--- src/Facebook/Provider.php
+++ src/Facebook/Provider.php
@@ -4,8 +4,9 @@ namespace SocialiteProviders\Facebook;
 
 use Laravel\Socialite\Two\FacebookProvider;
 use SocialiteProviders\Manager\ConfigTrait;
+use SocialiteProviders\Manager\Contracts\OAuth2\ProviderInterface;
 
-class Provider extends FacebookProvider
+class Provider extends FacebookProvider implements ProviderInterface
 {
     use ConfigTrait;
 
