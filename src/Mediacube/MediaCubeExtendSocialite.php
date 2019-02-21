adc7f990f6a44789feeb1d303dbf89b7dbc20fba
Kladislav <kim@mediacube.network>
2019-02-21 19:21:02 +0300

    add mediacube driver

---
+++ src/Mediacube/MediaCubeExtendSocialite.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace SocialiteProviders\MediaCube;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class LinkedInExtendSocialite
+{
+    /**
+     * Register the provider.
+     *
+     * @param \SocialiteProviders\Manager\SocialiteWasCalled $socialiteWasCalled
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite(
+            'mediacube', __NAMESPACE__.'\Provider'
+        );
+    }
+}
