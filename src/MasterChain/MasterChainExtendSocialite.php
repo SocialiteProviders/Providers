ee7522e2746ab25efa1f8a98b5aa516c8f2b534c
KevinXu <kevin830802@gmail.com>
2019-03-27 15:57:51 +0800

    :sparkles: Add MasterChain oAuth2 Provider

---
+++ src/MasterChain/MasterChainExtendSocialite.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace SocialiteProviders\MasterChain;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class MasterChainExtendSocialite
+{
+    /**
+     * Register the provider.
+     *
+     * @param \SocialiteProviders\Manager\SocialiteWasCalled $socialiteWasCalled
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite(
+            'masterchain', __NAMESPACE__.'\Provider'
+        );
+    }
+}
