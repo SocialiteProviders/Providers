09566e10a7f0a12418a9879b8c1fe7e1f0a6ee0a
sinkcup <sinkcup@gmail.com>
2019-04-26 18:45:25 +0800

    new: WeChat Web Provider with unionId

---
+++ src/WeChatWeb/Provider.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace SocialiteProviders\WeChatWeb;
+
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider
+{
+    /**
+     * Unique Provider Identifier.
+     */
+    const IDENTIFIER = 'WECHAT_WEB';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopes = ['snsapi_login'];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase('https://open.weixin.qq.com/connect/qrconnect', $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://api.weixin.qq.com/sns/oauth2/access_token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get('https://api.weixin.qq.com/sns/userinfo', [
+            'query' => [
+                'access_token' => $token, // HACK: Tencent use token in Query String, not in Header Authorization
+                'openid'       => $this->credentialsResponseBody['openid'],
+                'lang'         => 'zh_CN',
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            // HACK: use unionid as user id
+            'id'       => in_array('unionid', $this->getScopes()) ? $user['unionid'] : $user['openid'],
+             // HACK: Tencent scope snsapi_base only return openid
+            'nickname' => isset($user['nickname']) ? $user['nickname'] : null,
+            'name'     => null,
+            'email'    => null,
+            'avatar'   => isset($user['headimgurl']) ? $user['headimgurl'] : null,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return [
+            'appid' => $this->clientId,
+            'secret' => $this->clientSecret,
+            'code' => $code,
+            'grant_type' => 'authorization_code',
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getCodeFields($state = null)
+    {
+        $fields = parent::getCodeFields($state);
+        unset($fields['client_id']);
+        $fields['appid'] = $this->clientId; // HACK: Tencent use appid, not app_id or client_id
+
+        return $fields;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function formatScopes(array $scopes, $scopeSeparator)
+    {
+        // HACK: unionid is a faker scope for user id
+        if (in_array('unionid', $scopes)) {
+            unset($scopes[array_search('unionid', $scopes)]);
+        }
+        return implode($scopeSeparator, $scopes);
+    }
+}
