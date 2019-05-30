f4f59dd620ef55d76c3cd4d721faf50f7a5b7c39
Barry O'Donovan <barry@opensolutions.ie>
2019-05-30 12:08:42 +0100

    Add PeeringDB

---
+++ src/PeeringDB/Provider.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace SocialiteProviders\PeeringDB;
+
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider
+{
+    /**
+     * Unique Provider Identifier.
+     */
+    const IDENTIFIER = 'PEERINGDB';
+
+    /**
+     * Scopes defintions.
+     *
+     * @see http://developer.okta.com/docs/api/resources/oidc.html#scopes
+     */
+    const SCOPE_PROFILE  = 'profile';
+    const SCOPE_EMAIL    = 'email';
+    const SCOPE_NETWORKS = 'networks';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopes = ['profile email networks'];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase('https://auth.peeringdb.com/oauth2/authorize', $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://auth.peeringdb.com/oauth2/token/';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get('https://auth.peeringdb.com/profile/v1', [
+            'headers' => [
+                'Authorization' => 'Bearer '.$token,
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id'       => $user['id'],
+            'name'     => $user['name'],
+            'email'    => $user['email'],
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code'
+        ]);
+    }
+}
