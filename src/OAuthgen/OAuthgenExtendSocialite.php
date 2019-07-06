5cbc159eeaa41c543478017b97cf4281ec2f573a
ishan <ishan@vglobal.sg>
2019-07-06 16:08:39 +0500

    add OAuthgen provider

---
+++ src/OAuthgen/OAuthgenExtendSocialite.php
@@ -0,0 +1,15 @@
+<?php 
+namespace SocialiteProviders\OAuthgen;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class OAuthgenExtendSocialite
+{
+    /**
+     * Execute the provider.
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite('oauthgen', __NAMESPACE__.'\Provider');
+    }
+}
---
+++ src/OAuthgen/Provider.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace SocialiteProviders\OAuthgen;
+
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider
+{
+    /**
+     * Unique Provider Identifier.
+     */
+    const IDENTIFIER = 'OAUTHGEN';
+
+    /**
+     * The separating character for the requested scopes.
+     *
+     * @var string
+     */
+    protected $scopeSeparator = ' ';
+
+    /**
+     * BASE_URL
+     */
+    protected $oauth_url = 'https://auth.oauthgen.com';
+    protected $graph_url = 'https://graph.oauthgen.com/api/v1';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopes = ['blaze.graph.me', 'noconsent'];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase($this->oauth_url.'/oauth2/auth', $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return $this->oauth_url.'/oauth2/token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get($this->graph_url.'/me', [
+            'headers' => [
+                'Authorization' => 'Bearer '.$token,
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id'       => $user['id'],
+            'name'     => $user['name'],
+            'email'    => $user['email'],
+            'avatar'   => isset($user['avatar']) ? $user['avatar'] : null,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code'
+        ]);
+    }
+}
