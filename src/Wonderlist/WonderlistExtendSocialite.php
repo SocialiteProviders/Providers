1e6ae2a5f21f522b690b7331b1f1362361014818
andrewmherren <andrew.herren@gmail.com>
2019-07-27 12:17:26 -0700

    add wonderlist provider

---
+++ src/Wonderlist/WonderlistExtendSocialite.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace SocialiteProviders\Wonderlist;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class WonderlistExtendSocialite
+{
+    /**
+     * Register the provider.
+     *
+     * @param \SocialiteProviders\Manager\SocialiteWasCalled $socialiteWasCalled
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite(
+            'wonderlist', __NAMESPACE__.'\Provider'
+        );
+    }
+}
