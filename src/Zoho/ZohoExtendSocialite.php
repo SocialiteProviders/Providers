06069d42024f1164c69864e4f4be471163da13d7
mansoor <mansoor@glowlogix.com>
2019-08-10 16:58:50 +0500

    Zoho Provider

---
+++ src/Zoho/ZohoExtendSocialite.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SocialiteProviders\Zoho;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class ZohoExtendSocialite
+{
+    /**
+     * Execute the provider.
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite('zoho', __NAMESPACE__.'\Provider');
+    }
+}
