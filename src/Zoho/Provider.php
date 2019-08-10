39fa7278e7a1aab832a8953355acac4a97d37df2
mansoor <mansoor@glowlogix.com>
2019-08-10 16:58:50 +0500

    Zoho Provider

---
+++ src/Zoho/Provider.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace SocialiteProviders\Zoho;
+
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider
+{
+    /**
+     * Unique Provider Identifier.
+     */
+    const IDENTIFIER = 'ZOHO';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopes = ['aaaserver.profile.READ'];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase('https://accounts.zoho.com/oauth/v2/auth', $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://accounts.zoho.com/oauth/v2/token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get('https://accounts.zoho.com/oauth/user/info', [
+            'headers' => [
+                'Authorization' => 'Bearer '.$token,
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id' => $user['ZUID'],
+            'nickname' => $user['Display_Name'],
+            'name' => $user['First_Name'],
+            'avatar' => !empty($user['images']) ? $user['images'][0]['url'] : null,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code',
+        ]);
+    }
+}
