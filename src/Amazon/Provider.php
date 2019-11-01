cee9dd69543901a8bb74aac282ec5adb9a207568
atymic <atymicq@gmail.com>
2019-11-01 11:18:01 +1100

    feat: add amazon provider

---
+++ src/Amazon/Provider.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace SocialiteProviders\Amazon;
+
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider
+{
+    /**
+     * Unique Provider Identifier.
+     */
+    const IDENTIFIER = 'AMAZON';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopes = ['profile'];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase('https://www.amazon.com/ap/oa', $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://api.amazon.com/auth/o2/token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get('https://api.amazon.com/user/profile', [
+            'headers' => [
+                'Authorization' => "Bearer $token",
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id'       => $user['user_id'],
+            'nickname' => $user['name'],
+            'name'     => $user['name'],
+            'email'    => $user['email'],
+            'avatar'   => '',
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code'
+        ]);
+    }
+}
