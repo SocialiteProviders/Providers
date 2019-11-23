8fb9438526ffeffc72d32cfd631ab3d4d0ac9fcd
DB Chord <author@dbchord.com>
2019-11-23 19:04:11 +0700

    Retrieve a User with API V4
    Add:
    - Email field
    - Grant_type
    
    Remove:
    - [profile_image](https://github.com/SocialiteProviders/Providers/blob/b33270a1b9a935970f76771e0f46d7e4a00411f8/src/Bit.ly/Provider.php#L53)

--- src/Bit.ly/Provider.php
+++ src/Bit.ly/Provider.php
@@ -2,6 +2,7 @@
 
 namespace SocialiteProviders\Bitly;
 
+use Illuminate\Support\Arr;
 use SocialiteProviders\Manager\OAuth2\AbstractProvider;
 use SocialiteProviders\Manager\OAuth2\User;
 
@@ -36,10 +37,15 @@ class Provider extends AbstractProvider
     protected function getUserByToken($token)
     {
         $response = $this->getHttpClient()->get(
-            'https://api-ssl.bitly.com/v3/user/info?access_token='.$token
+            'https://api-ssl.bitly.com/v4/user', [
+                'headers'   => [
+                  'Accept' => 'application/json',
+                  'Authorization' => 'Bearer ' . $token,
+                ],
+            ]
         );
 
-        return json_decode($response->getBody()->getContents(), true)['data'];
+        return json_decode($response->getBody()->getContents(), true);
     }
 
     /**
@@ -48,9 +54,22 @@ class Provider extends AbstractProvider
     protected function mapUserToObject(array $user)
     {
         return (new User())->setRaw($user)->map([
-            'id'     => null, 'nickname' => $user['display_name'],
-            'name'   => $user['full_name'], 'email' => null,
-            'avatar' => $user['profile_image'],
+            'id'       => null,
+            'nickname' => $user['login'],
+            'name'     => $user['name'],
+            'email'    => Arr::collapse(Arr::where($user['emails'], function ($value, $key) {
+                return $value['is_primary'];
+            }))['email'],
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code',
         ]);
     }
 }
