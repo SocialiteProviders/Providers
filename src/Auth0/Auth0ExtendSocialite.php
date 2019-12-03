9593b65c6c1b2cae6781dcb0b30460e7030a7c40
Chase Coney <chasiepoo@gmail.com>
2019-12-02 23:05:22 -0800

    Add Auth0 socialite provider

---
+++ src/Auth0/Auth0ExtendSocialite.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SocialiteProviders\Auth0;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class Auth0ExtendSocialite
+{
+    /**
+     * Execute the provider.
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite('auth0', __NAMESPACE__.'\Provider');
+    }
+}
