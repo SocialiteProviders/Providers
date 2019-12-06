d5fce6a4579dc208628150889f875de2e3739563
Oleg Kuchumov <voenniy@gmail.com>
2019-12-06 15:13:31 +0300

    add keycloak provider

---
+++ src/Keycloak/Provider.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace SocialiteProviders\Keycloak;
+
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider
+{
+    /**
+     * Unique Provider Identifier.
+     */
+    const IDENTIFIER = 'KEYCLOAK';
+
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getBaseUrl()
+    {
+        return rtrim($this->getConfig('base_url') . '/realms/' . $this->getConfig('realms'), '/');
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase($this->getBaseUrl() . "/protocol/openid-connect/auth", $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return $this->getBaseUrl() . '/protocol/openid-connect/token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get($this->getBaseUrl() . '/protocol/openid-connect/userinfo', [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $token,
+            ],
+        ]);
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id' => $user['sub'],
+            'nickname' => $user['preferred_username'],
+            'name' => $user['given_name'],
+            'email' => $user['email'],
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code',
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function user($token = null)
+    {
+        if (!$token) {
+            return parent::user();
+        }
+        return $this->mapUserToObject($this->getUserByToken($token));
+    }
+}
