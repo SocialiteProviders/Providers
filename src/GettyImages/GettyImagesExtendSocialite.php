d69c6c6253d02b0892e1c4d2cb4d258474bd1fdc
bhuwan <bhuwan@inflcr.com>
2020-02-08 10:43:00 -0600

    added Getty Images in providers

---
+++ src/GettyImages/GettyImagesExtendSocialite.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SocialiteProviders\GetttyImages;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class GetttyImagesExtendSocialite
+{
+    /**
+     * Execute the provider.
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite('gettyimages', __NAMESPACE__.'\Provider');
+    }
+}
---
+++ src/GettyImages/Provider.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace SocialiteProviders\GetttyImages;
+
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider
+{   
+    /**
+     * Unique Provider Identifier.
+     */
+    const IDENTIFIER = 'GETTYIMAGES';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopes = [];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopeSeparator = ' ';
+
+    /**
+     * {@inheritdoc}
+     */
+    /**
+    * {@inheritdoc}
+    */
+   protected function getAuthUrl($state)
+   {
+       return $this->buildAuthUrlFromBase('https://api.gettyimages.com/oauth2/auth/', $state);
+   }
+
+   /**
+    * {@inheritdoc}
+    */
+   protected function getTokenUrl()
+   {
+       return 'https://api.gettyimages.com/oauth2/token';
+   }
+
+   /**
+    * {@inheritdoc}
+    */
+   public function getAccessToken($code)
+   {
+       $response = $this->getHttpClient()->post($this->getTokenUrl(), [
+           'headers' => ['Authorization' => 'Basic ' . base64_encode($this->clientId . ':' . $this->clientSecret)],
+           'body'    => $this->getTokenFields($code),
+       ]);
+
+       return json_decode($response->getBody()->getContents(), true);
+   }
+
+   /**
+    * {@inheritdoc}
+    */
+   protected function getTokenFields($code)
+   {
+       return array_merge(parent::getTokenFields($code), [
+           'grant_type' => 'authorization_code',
+       ]);
+   }
+
+   /**
+    * {@inheritdoc}
+    */
+   protected function getUserByToken($token)
+   {
+       $response = $this->getHttpClient()->get('https://api.gettyimages.com/v3/customers/current', [
+           'headers' => [
+               'Authorization' => 'Bearer ' . $token,
+               "Api-Key" =>  $this->clientId,
+           ],
+       ]);
+
+       return json_decode($response->getBody(), true);
+   }
+
+   /**
+    * {@inheritdoc}
+    */
+   protected function mapUserToObject(array $user)
+   {
+       return (new User)->setRaw($user)->map([
+           'first_name' => $user['first_name'],
+           'last_name'     => $user['last_name'],
+       ]);
+   }
+}
