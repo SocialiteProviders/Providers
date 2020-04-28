0cdeb2809823a064b4fb444884400198fbde5540
Jóhan Dam-Davidsen <johandavidsen@users.noreply.github.com>
2020-04-28 19:27:12 -0200

    [Asana ] Fixed error in the mapUserToObject function (#392)

--- src/Asana/Provider.php
+++ src/Asana/Provider.php
@@ -54,7 +54,7 @@ class Provider extends AbstractProvider
     protected function mapUserToObject(array $user)
     {
         return (new User())->setRaw($user)->map([
-            'id'    => $user['id'], 'nickname' => null, 'name' => $user['name'],
+            'id'    => $user['gid'], 'nickname' => null, 'name' => $user['name'],
             'email' => $user['email'], 'avatar' => $user['photo'],
         ]);
     }
