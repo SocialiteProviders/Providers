1de668acbf9b09b769715119b6c16ff346026f73
atymic <atymicq@gmail.com>
2020-05-03 20:41:50 +0000

    Apply fixes from StyleCI

--- src/GettyImages/Provider.php
+++ src/GettyImages/Provider.php
@@ -6,7 +6,7 @@ use SocialiteProviders\Manager\OAuth2\AbstractProvider;
 use SocialiteProviders\Manager\OAuth2\User;
 
 class Provider extends AbstractProvider
-{   
+{
     /**
      * Unique Provider Identifier.
      */
@@ -25,77 +25,81 @@ class Provider extends AbstractProvider
     /**
      * {@inheritdoc}
      */
+
     /**
-    * {@inheritdoc}
-    */
-   protected function getAuthUrl($state)
-   {
-       return $this->buildAuthUrlFromBase(
-           'https://api.gettyimages.com/oauth2/auth/', $state
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase(
+            'https://api.gettyimages.com/oauth2/auth/',
+            $state
         );
-   }
+    }
 
-   /**
-    * {@inheritdoc}
-    */
-   protected function getTokenUrl()
-   {
-       return 'https://api.gettyimages.com/oauth2/token';
-   }
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://api.gettyimages.com/oauth2/token';
+    }
 
-   /**
-    * {@inheritdoc}
-    */
-   public function getAccessToken($code)
-   {
-       $response = $this->getHttpClient()->post(
-           $this->getTokenUrl(), [
-           'headers' => [
-               'Authorization' => 'Basic ' . base64_encode(
-                   $this->clientId . ':' . $this->clientSecret
-                   )
-            ],
-           'body'    => $this->getTokenFields($code)
-           ]
+    /**
+     * {@inheritdoc}
+     */
+    public function getAccessToken($code)
+    {
+        $response = $this->getHttpClient()->post(
+            $this->getTokenUrl(),
+            [
+                'headers' => [
+                    'Authorization' => 'Basic '.base64_encode(
+                        $this->clientId.':'.$this->clientSecret
+                    ),
+                ],
+                'body'    => $this->getTokenFields($code),
+            ]
         );
 
-       return json_decode($response->getBody()->getContents(), true);
-   }
+        return json_decode($response->getBody()->getContents(), true);
+    }
 
-   /**
-    * {@inheritdoc}
-    */
-   protected function getTokenFields($code)
-   {
-       return array_merge(parent::getTokenFields($code), [
-           'grant_type' => 'authorization_code',
-       ]);
-   }
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code',
+        ]);
+    }
 
-   /**
-    * {@inheritdoc}
-    */
-   protected function getUserByToken($token)
-   {
-       $response = $this->getHttpClient()->get(
-           'https://api.gettyimages.com/v3/customers/current', 
-           [
-           'headers' => [
-               'Authorization' => 'Bearer ' . $token,
-               'Api-Key'  =>  $this->clientId,
-           ]]);
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get(
+            'https://api.gettyimages.com/v3/customers/current',
+            [
+                'headers' => [
+                    'Authorization' => 'Bearer '.$token,
+                    'Api-Key'       => $this->clientId,
+                ], ]
+        );
 
-       return json_decode($response->getBody(), true);
-   }
+        return json_decode($response->getBody(), true);
+    }
 
-   /**
-    * {@inheritdoc}
-    */
-   protected function mapUserToObject(array $user)
-   {
-       return (new User)->setRaw($user)->map([
-           'first_name' =>  $user['first_name'],
-           'last_name'  =>  $user['last_name'],
-       ]);
-   }
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'first_name' => $user['first_name'],
+            'last_name'  => $user['last_name'],
+        ]);
+    }
 }
