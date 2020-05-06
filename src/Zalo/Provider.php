cafd349f35a01f9014d012f1fdff0619dcb10c4a
atymic <atymicq@gmail.com>
2020-05-06 14:20:34 +1000

    chore: style fixes

--- src/Zalo/Provider.php
+++ src/Zalo/Provider.php
@@ -31,10 +31,11 @@ class Provider extends AbstractProvider
     protected function getCodeFields($state = null)
     {
         $fields = [
-            'app_id' => $this->clientId,
+            'app_id'       => $this->clientId,
             'redirect_uri' => $this->redirectUrl,
-            'state' => $state,
+            'state'        => $state,
         ];
+
         return array_merge($fields, $this->parameters);
     }
 
@@ -45,8 +46,9 @@ class Provider extends AbstractProvider
     {
         $response = $this->getHttpClient()->get($this->getTokenUrl(), [
             'headers' => ['Accept' => 'application/json'],
-            'query' => $this->getTokenFields($code),
+            'query'   => $this->getTokenFields($code),
         ]);
+
         return json_decode($response->getBody(), true);
     }
 
@@ -63,7 +65,7 @@ class Provider extends AbstractProvider
      */
     protected function getUserByToken($token)
     {
-        $response = $this->getHttpClient()->get('https://graph.zalo.me/v2.0/me?access_token=' . $token . '&fields=id,birthday,name,gender,picture');
+        $response = $this->getHttpClient()->get('https://graph.zalo.me/v2.0/me?access_token='.$token.'&fields=id,birthday,name,gender,picture');
 
         return json_decode($response->getBody(), true);
     }
@@ -74,10 +76,10 @@ class Provider extends AbstractProvider
     protected function mapUserToObject(array $user)
     {
         return (new User())->setRaw($user)->map([
-            'id' => $user['id'],
+            'id'       => $user['id'],
             'nickname' => null,
-            'name' => $user['name'],
-            'avatar' => preg_replace('/^http:/i', 'https:', $user['picture']['data']['url']),
+            'name'     => $user['name'],
+            'avatar'   => preg_replace('/^http:/i', 'https:', $user['picture']['data']['url']),
         ]);
     }
 
@@ -87,9 +89,9 @@ class Provider extends AbstractProvider
     protected function getTokenFields($code)
     {
         return [
-            'app_id' => $this->clientId,
-            'app_secret' => $this->clientSecret,
-            'code' => $code,
+            'app_id'       => $this->clientId,
+            'app_secret'   => $this->clientSecret,
+            'code'         => $code,
             'redirect_uri' => $this->redirectUrl,
         ];
     }
