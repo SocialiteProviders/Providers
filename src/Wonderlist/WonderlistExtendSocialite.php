d9af7fc92007941ff8e4eea9b324b1190342da33
atymic <atymicq@gmail.com>
2020-05-14 11:19:43 +1000

    fix: deprecate wunderlist

--- src/Wonderlist/WonderlistExtendSocialite.php
+++ src/Wonderlist/WonderlistExtendSocialite.php
@@ -4,6 +4,10 @@ namespace SocialiteProviders\Wonderlist;
 
 use SocialiteProviders\Manager\SocialiteWasCalled;
 
+/**
+ * @deprecated Wunderlist is shutting down
+ * @see        https://www.wunderlist.com/blog/goodbye-from-wunderlist/
+ */
 class WonderlistExtendSocialite
 {
     /**
