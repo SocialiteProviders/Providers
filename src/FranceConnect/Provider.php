c2425fe206da5f08e048d3f6fb8ec6d4395cbe43
akachbat <akachbat@gmail.com>
2020-05-20 15:57:54 +0000

    style fixes

--- src/FranceConnect/Provider.php
+++ src/FranceConnect/Provider.php
@@ -7,7 +7,7 @@ use SocialiteProviders\Manager\OAuth2\AbstractProvider;
 class Provider extends AbstractProvider
 {
     /**
-     * TEST API URL
+     * TEST API URL.
      */
     const TEST_AUTHORIZE_URL = 'https://fcp.integ01.dev-franceconnect.fr/api/v1/authorize';
     const TEST_TOKEN_URL = 'https://fcp.integ01.dev-franceconnect.fr/api/v1/token';
@@ -15,7 +15,7 @@ class Provider extends AbstractProvider
     const TEST_LOGOUT_URL = 'https://fcp.integ01.dev-franceconnect.fr/api/v1/logout';
 
     /**
-     * PROD API URL
+     * PROD API URL.
      */
     const PROD_AUTHORIZE_URL = 'https://app.franceconnect.gouv.fr/api/v1/authorize';
     const PROD_TOKEN_URL = 'https://app.franceconnect.gouv.fr/api/v1/token';
@@ -26,7 +26,7 @@ class Provider extends AbstractProvider
      * Unique Provider Identifier.
      */
     const IDENTIFIER = 'FRANCECONNECT';
-    
+
     /**
      * The scopes being requested.
      *
@@ -40,20 +40,19 @@ class Provider extends AbstractProvider
         'birthplace',
         'birthcountry',
         'email',
-        'preferred_username'
+        'preferred_username',
     ];
 
     /**
      * {@inheritdoc}
      */
     protected $scopeSeparator = ' ';
-    
-    
+
     /**
      * {@inheritdoc}
      */
     protected function getAuthUrl($state)
-    {   
+    {
         //It is used to prevent replay attacks
         $this->parameters['nonce'] = str_random(20);
 
@@ -66,10 +65,10 @@ class Provider extends AbstractProvider
     public function getAccessTokenResponse($code)
     {
         $response = $this->getHttpClient()->post($this->getTokenUrl(), [
-            'headers'     => ['Authorization' => 'Basic ' . base64_encode($this->clientId . ':' . $this->clientSecret)],
+            'headers'     => ['Authorization' => 'Basic '.base64_encode($this->clientId.':'.$this->clientSecret)],
             'form_params' => $this->getTokenFields($code),
         ]);
-        
+
         return json_decode($response->getBody(), true);
     }
 
@@ -79,17 +78,19 @@ class Provider extends AbstractProvider
     protected function getTokenFields($code)
     {
         return array_add(
-            parent::getTokenFields($code), 'grant_type', 'authorization_code'
+            parent::getTokenFields($code),
+            'grant_type',
+            'authorization_code'
         );
     }
-    
+
     /**
      * {@inheritdoc}
      */
     public function user()
     {
         if ($this->hasInvalidState()) {
-            throw new InvalidStateException;
+            throw new InvalidStateException();
         }
 
         $response = $this->getAccessTokenResponse($this->getCode());
@@ -108,10 +109,10 @@ class Provider extends AbstractProvider
      * {@inheritdoc}
      */
     protected function getUserByToken($token)
-    {        
+    {
         $response = $this->getHttpClient()->get($this->getUserInfoUrl(), [
             'headers' => [
-                'Authorization' => 'Bearer ' . $token,
+                'Authorization' => 'Bearer '.$token,
             ],
         ]);
 
@@ -122,39 +123,39 @@ class Provider extends AbstractProvider
      * {@inheritdoc}
      */
     protected function mapUserToObject(array $user)
-    {        
-        return (new User)->setRaw($user)->map([
-            'id'       => $user['sub'],
-            'given_name' => $user['given_name'],
-            'family_name'     => $user['family_name'],
-            'gender'     => $user['gender'],
-            'birthplace'     => $user['birthplace'],
-            'birthcountry'     => $user['birthcountry'],
-            'email'     => $user['email'],
-            'preferred_username'     => $user['preferred_username']
+    {
+        return (new User())->setRaw($user)->map([
+            'id'                     => $user['sub'],
+            'given_name'             => $user['given_name'],
+            'family_name'            => $user['family_name'],
+            'gender'                 => $user['gender'],
+            'birthplace'             => $user['birthplace'],
+            'birthcountry'           => $user['birthcountry'],
+            'email'                  => $user['email'],
+            'preferred_username'     => $user['preferred_username'],
         ]);
     }
 
     /**
-     * Generate  URL for Logout
-     * 
-     * @param  string  $tokenId
+     * Generate  URL for Logout.
+     *
+     * @param string $tokenId
+     *
      * @return string
      */
-
     public function generateLogoutURL($tokenId)
     {
         $params = [
             'post_logout_redirect_uri' => config('services.france_connect.logout_redirect'),
-            'id_token_hint' => $tokenId
+            'id_token_hint'            => $tokenId,
         ];
 
-        return $this->getLogoutUrl().'?'.http_build_query($params);      
+        return $this->getLogoutUrl().'?'.http_build_query($params);
     }
 
     /**
-     * Return API Authorize URL
-     * 
+     * Return API Authorize URL.
+     *
      * @return string
      */
     protected function getAuthorizeUrl()
@@ -163,14 +164,14 @@ class Provider extends AbstractProvider
     }
 
     /**
-     * Return API User Info URL
-     * 
+     * Return API User Info URL.
+     *
      * @return string
      */
     protected function getUserInfoUrl()
     {
         return config('app.env') == 'production' ? self::PROD_USERINFO_URL : self::TEST_USERINFO_URL;
-    }    
+    }
 
     /**
      * {@inheritdoc}
@@ -187,6 +188,4 @@ class Provider extends AbstractProvider
     {
         return config('app.env') == 'production' ? self::PROD_LOGOUT_URL : self::TEST_LOGOUT_URL;
     }
-    
-    
 }
--- src/FranceConnect/User.php
+++ src/FranceConnect/User.php
@@ -7,7 +7,7 @@ use Laravel\Socialite\AbstractUser;
 class User extends AbstractUser
 {
     /**
-     * The user's id token hint (serve for logout)
+     * The user's id token hint (serve for logout).
      */
     public $tokenId;
 
@@ -35,10 +35,10 @@ class User extends AbstractUser
     /**
      * Set the token id on the user.
      *
-     * @param  string  $tokenId
+     * @param string $tokenId
+     *
      * @return $this
      */
-
     public function setTokenId($tokenId)
     {
         $this->tokenId = $tokenId;
@@ -49,7 +49,8 @@ class User extends AbstractUser
     /**
      * Set the token on the user.
      *
-     * @param  string  $token
+     * @param string $token
+     *
      * @return $this
      */
     public function setToken($token)
@@ -62,7 +63,8 @@ class User extends AbstractUser
     /**
      * Set the refresh token required to obtain a new access token.
      *
-     * @param  string  $refreshToken
+     * @param string $refreshToken
+     *
      * @return $this
      */
     public function setRefreshToken($refreshToken)
@@ -75,7 +77,8 @@ class User extends AbstractUser
     /**
      * Set the number of seconds the access token is valid for.
      *
-     * @param  int  $expiresIn
+     * @param int $expiresIn
+     *
      * @return $this
      */
     public function setExpiresIn($expiresIn)
