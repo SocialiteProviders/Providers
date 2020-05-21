42ec17b40ba935b91de4a83ac9609e35199c4261
Luke Curtis <lukesimoncurtis@gmail.com>
2020-05-21 15:59:28 +0100

    :rocket: Quickbooks oAuth 2 Update

--- src/QuickBooks/Provider.php
+++ src/QuickBooks/Provider.php
@@ -1,16 +1,68 @@
 <?php
 
-namespace SocialiteProviders\QuickBooks;
+namespace SocialiteProviders\Quickbooks2;
 
-use SocialiteProviders\Manager\OAuth1\AbstractProvider;
-use SocialiteProviders\Manager\OAuth1\User;
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+use Laravel\Socialite\Two\ProviderInterface;
+use SocialiteProviders\Manager\ConfigTrait;
 
-class Provider extends AbstractProvider
+class Provider extends AbstractProvider implements ProviderInterface
 {
+    use ConfigTrait;
+
     /**
      * Unique Provider Identifier.
      */
     const IDENTIFIER = 'QUICKBOOKS';
+
+    /**
+     * Sandbox endpoint for retrieving user info.
+     */
+    const USERSANDBOXENDPOINT = 'https://sandbox-accounts.platform.intuit.com/v1/openid_connect/userinfo';
+
+    /**
+     * Production endpoint for retrieving user info.
+     */
+    const USERPRODUCTIONENDPOINT = 'https://accounts.platform.intuit.com/v1/openid_connect/userinfo';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopes = [];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase('https://appcenter.intuit.com/connect/oauth2', $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $endpoint = $this->getConfig('app.env', 'local') == 'production' ? self::USERPRODUCTIONENDPOINT : self::USERSANDBOXENDPOINT;
+
+        $response = $this->getHttpClient()->get($endpoint, [
+            'headers' => [
+                'Accept' => 'application/json',
+                'Authorization' => 'Bearer '.$token,
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
 
     /**
      * {@inheritdoc}
@@ -18,9 +70,19 @@ class Provider extends AbstractProvider
     protected function mapUserToObject(array $user)
     {
         return (new User())->setRaw($user)->map([
-            'id'    => $user['id'],
-            'name'  => $user['name'],
-            'email' => $user['email'],
+            'id'       => $user['sub'],
+            'name'     => $user['givenName'] ?? '' . ' ' . $user['familyName'] ?? '',
+            'email'    => $user['email'],
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code'
         ]);
     }
 }
