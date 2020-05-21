4b618a0e3f1b6c12f9949d92655769c29925dbfb
Luke Curtis <lukesimoncurtis@gmail.com>
2020-05-21 16:01:44 +0100

    fix namespace\

--- src/QuickBooks/Provider.php
+++ src/QuickBooks/Provider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace SocialiteProviders\Quickbooks;
+namespace SocialiteProviders\QuickBooks;
 
 use SocialiteProviders\Manager\OAuth2\AbstractProvider;
 use SocialiteProviders\Manager\OAuth2\User;
