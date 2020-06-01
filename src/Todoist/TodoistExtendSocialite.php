2e9020b37dd6453ca89c7a6f88fc7948fbffb1ee
atymic <atymicq@gmail.com>
2020-06-01 21:13:57 +1000

    feat: add todoist provider (#431)

---
+++ src/Todoist/TodoistExtendSocialite.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SocialiteProviders\Todoist;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class TodoistExtendSocialite
+{
+    /**
+     * Execute the provider.
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite('todoist', __NAMESPACE__.'\Provider');
+    }
+}
