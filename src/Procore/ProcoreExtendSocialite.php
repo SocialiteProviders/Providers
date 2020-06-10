9dc5e8a64d99942b43cfec83920a15cef84e411b
Christopher Forrence <cforrence@oxblue.com>
2020-06-10 10:41:25 -0400

    Adds Procore provider

---
+++ src/Procore/ProcoreExtendSocialite.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace SocialiteProviders\Procore;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class ProcoreExtendSocialite
+{
+    /**
+     * Register the provider.
+     *
+     * @param \SocialiteProviders\Manager\SocialiteWasCalled $socialiteWasCalled
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite(
+            'procore',
+            __NAMESPACE__ . '\Provider'
+        );
+    }
+}
