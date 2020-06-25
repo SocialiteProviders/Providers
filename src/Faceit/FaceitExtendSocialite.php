c2583a70dd8cde496637caa607feb7be51352591
SofWar <ya.sofwar@yandex.com>
2020-06-25 17:57:42 +0300

    add provider Faceit

---
+++ src/Faceit/FaceitExtendSocialite.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SocialiteProviders\Faceit;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class FaceitExtendSocialite
+{
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite('faceit', __NAMESPACE__ . '\Provider');
+    }
+}
