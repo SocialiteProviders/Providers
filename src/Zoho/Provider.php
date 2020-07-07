ae4d97f34040c0f0d11c8634d3876cb145eeae03
Ajay Dwivedi <talk2adwiv@gmail.com>
2020-07-07 17:30:37 +0530

    Fix issue #448 (#449)

--- src/Zoho/Provider.php
+++ src/Zoho/Provider.php
@@ -54,8 +54,9 @@ class Provider extends AbstractProvider
     {
         return (new User())->setRaw($user)->map([
             'id'       => $user['ZUID'],
+            'email'    => $user['Email'],
             'nickname' => $user['Display_Name'],
-            'name'     => $user['First_Name'],
+            'name'     => $user['First_Name'].' '.$user['Last_Name'],
             'avatar'   => !empty($user['images']) ? $user['images'][0]['url'] : null,
         ]);
     }
