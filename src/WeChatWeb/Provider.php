2fb83e2909329e147f9ab0ca942b8b3751f10d48
Lucas Michot <lucas.michot@shopify.com>
2020-07-27 09:46:29 +0200

    Use strict check with in_array function

--- src/WeChatWeb/Provider.php
+++ src/WeChatWeb/Provider.php
@@ -59,7 +59,7 @@ class Provider extends AbstractProvider
     {
         return (new User())->setRaw($user)->map([
             // HACK: use unionid as user id
-            'id'       => in_array('unionid', $this->getScopes()) ? $user['unionid'] : $user['openid'],
+            'id'       => in_array('unionid', $this->getScopes(), true) ? $user['unionid'] : $user['openid'],
             // HACK: Tencent scope snsapi_base only return openid
             'nickname' => isset($user['nickname']) ? $user['nickname'] : null,
             'name'     => null,
@@ -99,7 +99,7 @@ class Provider extends AbstractProvider
     protected function formatScopes(array $scopes, $scopeSeparator)
     {
         // HACK: unionid is a faker scope for user id
-        if (in_array('unionid', $scopes)) {
+        if (in_array('unionid', $scopes, true)) {
             unset($scopes[array_search('unionid', $scopes)]);
         }
 
