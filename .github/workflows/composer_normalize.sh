9cac5da9b09f2dd1d0e8eec7b7fbec81397302f2
Lucas Michot <lucas.michot@shopify.com>
2020-08-01 10:35:47 +0200

    prefer diff for errors

--- .github/workflows/composer_normalize.sh
+++ .github/workflows/composer_normalize.sh
@@ -1,169 +1,169 @@
 #!/usr/bin/env bash
 
-composer normalize composer.json
-composer normalize src/Acclaim/composer.json
-composer normalize src/Admitad/composer.json
-composer normalize src/Amazon/composer.json
-composer normalize src/AngelList/composer.json
-composer normalize src/AppNet/composer.json
-composer normalize src/Apple/composer.json
-composer normalize src/ArcGIS/composer.json
-composer normalize src/Asana/composer.json
-composer normalize src/Auth0/composer.json
-composer normalize src/Aweber/composer.json
-composer normalize src/Azure/composer.json
-composer normalize src/Battlenet/composer.json
-composer normalize src/Bitbucket/composer.json
-composer normalize src/Bitly/composer.json
-composer normalize src/Box/composer.json
-composer normalize src/Buffer/composer.json
-composer normalize src/CampaignMonitor/composer.json
-composer normalize src/Cheddar/composer.json
-composer normalize src/ClaveUnica/composer.json
-composer normalize src/Coinbase/composer.json
-composer normalize src/ConstantContact/composer.json
-composer normalize src/Coursera/composer.json
-composer normalize src/Dailymotion/composer.json
-composer normalize src/Dataporten/composer.json
-composer normalize src/Deezer/composer.json
-composer normalize src/Deviantart/composer.json
-composer normalize src/DigitalOcean/composer.json
-composer normalize src/Discogs/composer.json
-composer normalize src/Discord/composer.json
-composer normalize src/Disqus/composer.json
-composer normalize src/Douban/composer.json
-composer normalize src/Dribbble/composer.json
-composer normalize src/Dropbox/composer.json
-composer normalize src/Envato/composer.json
-composer normalize src/Etsy/composer.json
-composer normalize src/Eventbrite/composer.json
-composer normalize src/EyeEm/composer.json
-composer normalize src/Facebook/composer.json
-composer normalize src/Faceit/composer.json
-composer normalize src/Fitbit/composer.json
-composer normalize src/FiveHundredPixel/composer.json
-composer normalize src/Flattr/composer.json
-composer normalize src/Flexkids/composer.json
-composer normalize src/Flickr/composer.json
-composer normalize src/Foursquare/composer.json
-composer normalize src/FranceConnect/composer.json
-composer normalize src/GarminConnect/composer.json
-composer normalize src/GettyImages/composer.json
-composer normalize src/GitHub/composer.json
-composer normalize src/GitLab/composer.json
-composer normalize src/Gitee/composer.json
-composer normalize src/Goodreads/composer.json
-composer normalize src/Google/composer.json
-composer normalize src/Graph/composer.json
-composer normalize src/Harvest/composer.json
-composer normalize src/HeadHunter/composer.json
-composer normalize src/Heroku/composer.json
-composer normalize src/HubSpot/composer.json
-composer normalize src/HumanApi/composer.json
-composer normalize src/IFSP/composer.json
-composer normalize src/Imgur/composer.json
-composer normalize src/Instagram/composer.json
-composer normalize src/InstagramBasic/composer.json
-composer normalize src/Intercom/composer.json
-composer normalize src/Jira/composer.json
-composer normalize src/Kakao/composer.json
-composer normalize src/Keycloak/composer.json
-composer normalize src/LaravelPassport/composer.json
-composer normalize src/Line/composer.json
-composer normalize src/LinkedIn/composer.json
-composer normalize src/Live/composer.json
-composer normalize src/MailChimp/composer.json
-composer normalize src/Mailru/composer.json
-composer normalize src/MakerLog/composer.json
-composer normalize src/Mattermost/composer.json
-composer normalize src/MediaCube/composer.json
-composer normalize src/Medium/composer.json
-composer normalize src/Meetup/composer.json
-composer normalize src/Microsoft/composer.json
-composer normalize src/Mixcloud/composer.json
-composer normalize src/MoiKrug/composer.json
-composer normalize src/Mollie/composer.json
-composer normalize src/Naver/composer.json
-composer normalize src/Netlify/composer.json
-composer normalize src/Nocks/composer.json
-composer normalize src/OAuthgen/composer.json
-composer normalize src/OSChina/composer.json
-composer normalize src/Odnoklassniki/composer.json
-composer normalize src/Okta/composer.json
-composer normalize src/Orcid/composer.json
-composer normalize src/Patreon/composer.json
-composer normalize src/PayPal/composer.json
-composer normalize src/PayPalSandbox/composer.json
-composer normalize src/Paymill/composer.json
-composer normalize src/PeeringDB/composer.json
-composer normalize src/Pinterest/composer.json
-composer normalize src/Pipedrive/composer.json
-composer normalize src/Podio/composer.json
-composer normalize src/Procore/composer.json
-composer normalize src/ProductHunt/composer.json
-composer normalize src/ProjectV/composer.json
-composer normalize src/Pushbullet/composer.json
-composer normalize src/QQ/composer.json
-composer normalize src/QuickBooks/composer.json
-composer normalize src/Readability/composer.json
-composer normalize src/Redbooth/composer.json
-composer normalize src/Reddit/composer.json
-composer normalize src/RunKeeper/composer.json
-composer normalize src/SURFconext/composer.json
-composer normalize src/Sage/composer.json
-composer normalize src/SalesForce/composer.json
-composer normalize src/SciStarter/composer.json
-composer normalize src/SharePoint/composer.json
-composer normalize src/Shopify/composer.json
-composer normalize src/Slack/composer.json
-composer normalize src/Smashcast/composer.json
-composer normalize src/Snapchat/composer.json
-composer normalize src/SoundCloud/composer.json
-composer normalize src/Spotify/composer.json
-composer normalize src/StackExchange/composer.json
-composer normalize src/Steam/composer.json
-composer normalize src/Steem/composer.json
-composer normalize src/StockTwits/composer.json
-composer normalize src/Strava/composer.json
-composer normalize src/Stripe/composer.json
-composer normalize src/TVShowTime/composer.json
-composer normalize src/TeamService/composer.json
-composer normalize src/Teamleader/composer.json
-composer normalize src/Teamweek/composer.json
-composer normalize src/ThirtySevenSignals/composer.json
-composer normalize src/Todoist/composer.json
-composer normalize src/Trakt/composer.json
-composer normalize src/Trello/composer.json
-composer normalize src/Tumblr/composer.json
-composer normalize src/TwentyThreeAndMe/composer.json
-composer normalize src/Twitch/composer.json
-composer normalize src/Twitter/composer.json
-composer normalize src/UCL/composer.json
-composer normalize src/UFS/composer.json
-composer normalize src/Uber/composer.json
-composer normalize src/Ufutx/composer.json
-composer normalize src/Unsplash/composer.json
-composer normalize src/Untappd/composer.json
-composer normalize src/VKontakte/composer.json
-composer normalize src/Venmo/composer.json
-composer normalize src/Vercel/composer.json
-composer normalize src/VersionOne/composer.json
-composer normalize src/Vimeo/composer.json
-composer normalize src/WeChatServiceAccount/composer.json
-composer normalize src/WeChatWeb/composer.json
-composer normalize src/Weibo/composer.json
-composer normalize src/Weixin/composer.json
-composer normalize src/WeixinWeb/composer.json
-composer normalize src/Withings/composer.json
-composer normalize src/WordPress/composer.json
-composer normalize src/Xero/composer.json
-composer normalize src/Xing/composer.json
-composer normalize src/Yahoo/composer.json
-composer normalize src/Yammer/composer.json
-composer normalize src/Yandex/composer.json
-composer normalize src/Yiban/composer.json
-composer normalize src/YouTube/composer.json
-composer normalize src/Zalo/composer.json
-composer normalize src/Zendesk/composer.json
-composer normalize src/Zoho/composer.json
-composer normalize src/xREL/composer.json
+composer normalize --diff composer.json
+composer normalize --diff src/Acclaim/composer.json
+composer normalize --diff src/Admitad/composer.json
+composer normalize --diff src/Amazon/composer.json
+composer normalize --diff src/AngelList/composer.json
+composer normalize --diff src/AppNet/composer.json
+composer normalize --diff src/Apple/composer.json
+composer normalize --diff src/ArcGIS/composer.json
+composer normalize --diff src/Asana/composer.json
+composer normalize --diff src/Auth0/composer.json
+composer normalize --diff src/Aweber/composer.json
+composer normalize --diff src/Azure/composer.json
+composer normalize --diff src/Battlenet/composer.json
+composer normalize --diff src/Bitbucket/composer.json
+composer normalize --diff src/Bitly/composer.json
+composer normalize --diff src/Box/composer.json
+composer normalize --diff src/Buffer/composer.json
+composer normalize --diff src/CampaignMonitor/composer.json
+composer normalize --diff src/Cheddar/composer.json
+composer normalize --diff src/ClaveUnica/composer.json
+composer normalize --diff src/Coinbase/composer.json
+composer normalize --diff src/ConstantContact/composer.json
+composer normalize --diff src/Coursera/composer.json
+composer normalize --diff src/Dailymotion/composer.json
+composer normalize --diff src/Dataporten/composer.json
+composer normalize --diff src/Deezer/composer.json
+composer normalize --diff src/Deviantart/composer.json
+composer normalize --diff src/DigitalOcean/composer.json
+composer normalize --diff src/Discogs/composer.json
+composer normalize --diff src/Discord/composer.json
+composer normalize --diff src/Disqus/composer.json
+composer normalize --diff src/Douban/composer.json
+composer normalize --diff src/Dribbble/composer.json
+composer normalize --diff src/Dropbox/composer.json
+composer normalize --diff src/Envato/composer.json
+composer normalize --diff src/Etsy/composer.json
+composer normalize --diff src/Eventbrite/composer.json
+composer normalize --diff src/EyeEm/composer.json
+composer normalize --diff src/Facebook/composer.json
+composer normalize --diff src/Faceit/composer.json
+composer normalize --diff src/Fitbit/composer.json
+composer normalize --diff src/FiveHundredPixel/composer.json
+composer normalize --diff src/Flattr/composer.json
+composer normalize --diff src/Flexkids/composer.json
+composer normalize --diff src/Flickr/composer.json
+composer normalize --diff src/Foursquare/composer.json
+composer normalize --diff src/FranceConnect/composer.json
+composer normalize --diff src/GarminConnect/composer.json
+composer normalize --diff src/GettyImages/composer.json
+composer normalize --diff src/GitHub/composer.json
+composer normalize --diff src/GitLab/composer.json
+composer normalize --diff src/Gitee/composer.json
+composer normalize --diff src/Goodreads/composer.json
+composer normalize --diff src/Google/composer.json
+composer normalize --diff src/Graph/composer.json
+composer normalize --diff src/Harvest/composer.json
+composer normalize --diff src/HeadHunter/composer.json
+composer normalize --diff src/Heroku/composer.json
+composer normalize --diff src/HubSpot/composer.json
+composer normalize --diff src/HumanApi/composer.json
+composer normalize --diff src/IFSP/composer.json
+composer normalize --diff src/Imgur/composer.json
+composer normalize --diff src/Instagram/composer.json
+composer normalize --diff src/InstagramBasic/composer.json
+composer normalize --diff src/Intercom/composer.json
+composer normalize --diff src/Jira/composer.json
+composer normalize --diff src/Kakao/composer.json
+composer normalize --diff src/Keycloak/composer.json
+composer normalize --diff src/LaravelPassport/composer.json
+composer normalize --diff src/Line/composer.json
+composer normalize --diff src/LinkedIn/composer.json
+composer normalize --diff src/Live/composer.json
+composer normalize --diff src/MailChimp/composer.json
+composer normalize --diff src/Mailru/composer.json
+composer normalize --diff src/MakerLog/composer.json
+composer normalize --diff src/Mattermost/composer.json
+composer normalize --diff src/MediaCube/composer.json
+composer normalize --diff src/Medium/composer.json
+composer normalize --diff src/Meetup/composer.json
+composer normalize --diff src/Microsoft/composer.json
+composer normalize --diff src/Mixcloud/composer.json
+composer normalize --diff src/MoiKrug/composer.json
+composer normalize --diff src/Mollie/composer.json
+composer normalize --diff src/Naver/composer.json
+composer normalize --diff src/Netlify/composer.json
+composer normalize --diff src/Nocks/composer.json
+composer normalize --diff src/OAuthgen/composer.json
+composer normalize --diff src/OSChina/composer.json
+composer normalize --diff src/Odnoklassniki/composer.json
+composer normalize --diff src/Okta/composer.json
+composer normalize --diff src/Orcid/composer.json
+composer normalize --diff src/Patreon/composer.json
+composer normalize --diff src/PayPal/composer.json
+composer normalize --diff src/PayPalSandbox/composer.json
+composer normalize --diff src/Paymill/composer.json
+composer normalize --diff src/PeeringDB/composer.json
+composer normalize --diff src/Pinterest/composer.json
+composer normalize --diff src/Pipedrive/composer.json
+composer normalize --diff src/Podio/composer.json
+composer normalize --diff src/Procore/composer.json
+composer normalize --diff src/ProductHunt/composer.json
+composer normalize --diff src/ProjectV/composer.json
+composer normalize --diff src/Pushbullet/composer.json
+composer normalize --diff src/QQ/composer.json
+composer normalize --diff src/QuickBooks/composer.json
+composer normalize --diff src/Readability/composer.json
+composer normalize --diff src/Redbooth/composer.json
+composer normalize --diff src/Reddit/composer.json
+composer normalize --diff src/RunKeeper/composer.json
+composer normalize --diff src/SURFconext/composer.json
+composer normalize --diff src/Sage/composer.json
+composer normalize --diff src/SalesForce/composer.json
+composer normalize --diff src/SciStarter/composer.json
+composer normalize --diff src/SharePoint/composer.json
+composer normalize --diff src/Shopify/composer.json
+composer normalize --diff src/Slack/composer.json
+composer normalize --diff src/Smashcast/composer.json
+composer normalize --diff src/Snapchat/composer.json
+composer normalize --diff src/SoundCloud/composer.json
+composer normalize --diff src/Spotify/composer.json
+composer normalize --diff src/StackExchange/composer.json
+composer normalize --diff src/Steam/composer.json
+composer normalize --diff src/Steem/composer.json
+composer normalize --diff src/StockTwits/composer.json
+composer normalize --diff src/Strava/composer.json
+composer normalize --diff src/Stripe/composer.json
+composer normalize --diff src/TVShowTime/composer.json
+composer normalize --diff src/TeamService/composer.json
+composer normalize --diff src/Teamleader/composer.json
+composer normalize --diff src/Teamweek/composer.json
+composer normalize --diff src/ThirtySevenSignals/composer.json
+composer normalize --diff src/Todoist/composer.json
+composer normalize --diff src/Trakt/composer.json
+composer normalize --diff src/Trello/composer.json
+composer normalize --diff src/Tumblr/composer.json
+composer normalize --diff src/TwentyThreeAndMe/composer.json
+composer normalize --diff src/Twitch/composer.json
+composer normalize --diff src/Twitter/composer.json
+composer normalize --diff src/UCL/composer.json
+composer normalize --diff src/UFS/composer.json
+composer normalize --diff src/Uber/composer.json
+composer normalize --diff src/Ufutx/composer.json
+composer normalize --diff src/Unsplash/composer.json
+composer normalize --diff src/Untappd/composer.json
+composer normalize --diff src/VKontakte/composer.json
+composer normalize --diff src/Venmo/composer.json
+composer normalize --diff src/Vercel/composer.json
+composer normalize --diff src/VersionOne/composer.json
+composer normalize --diff src/Vimeo/composer.json
+composer normalize --diff src/WeChatServiceAccount/composer.json
+composer normalize --diff src/WeChatWeb/composer.json
+composer normalize --diff src/Weibo/composer.json
+composer normalize --diff src/Weixin/composer.json
+composer normalize --diff src/WeixinWeb/composer.json
+composer normalize --diff src/Withings/composer.json
+composer normalize --diff src/WordPress/composer.json
+composer normalize --diff src/Xero/composer.json
+composer normalize --diff src/Xing/composer.json
+composer normalize --diff src/Yahoo/composer.json
+composer normalize --diff src/Yammer/composer.json
+composer normalize --diff src/Yandex/composer.json
+composer normalize --diff src/Yiban/composer.json
+composer normalize --diff src/YouTube/composer.json
+composer normalize --diff src/Zalo/composer.json
+composer normalize --diff src/Zendesk/composer.json
+composer normalize --diff src/Zoho/composer.json
+composer normalize --diff src/xREL/composer.json
