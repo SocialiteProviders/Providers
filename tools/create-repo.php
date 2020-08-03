c5fc2f9979f12f98a8e2695cc910e118dc03b55c
atymic <atymicq@gmail.com>
2020-08-03 13:01:10 +1000

    chore: style fixes

--- tools/create-repo.php
+++ tools/create-repo.php
@@ -1,10 +1,9 @@
 <?php
 
 /**
- * Create a new repo with preset information
+ * Create a new repo with preset information.
  */
-
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
 $repoName = $argv[1] ?? null;
 
@@ -14,13 +13,13 @@ if (empty($repoName)) {
 }
 
 $res = \Zttp\Zttp::withHeaders([
-    'Accept' => 'application/vnd.github.v3+json',
-    'Authorization' => 'token ' . getenv('GITHUB_TOKEN'),
+    'Accept'        => 'application/vnd.github.v3+json',
+    'Authorization' => 'token '.getenv('GITHUB_TOKEN'),
 ])->post('https://api.github.com/orgs/SocialiteProviders/repos', [
-    'name' => $repoName,
+    'name'        => $repoName,
     'description' => sprintf('[READ ONLY] Subtree split of the SocialiteProviders/%s Provider (see SocialiteProviders/Providers)', $repoName),
-    'homepage' => sprintf('https://socialiteproviders.com/%s/', $repoName),
-    'has_issues' => false,
+    'homepage'    => sprintf('https://socialiteproviders.com/%s/', $repoName),
+    'has_issues'  => false,
 ]);
 
 echo sprintf("Created Repo: %s, response: %s\n", $res->status() !== 200 ? $res->body() : $res->json()['full_name'], $res->status());
@@ -30,9 +29,9 @@ if ($res->status() !== 200) {
 }
 
 $res = \Zttp\Zttp::withHeaders([
-    'Accept' => 'application/vnd.github.mercy-preview+json',
-    'Authorization' => 'token ' . getenv('GITHUB_TOKEN'),
-])->put($res->json()['url'] . '/topics', [
+    'Accept'        => 'application/vnd.github.mercy-preview+json',
+    'Authorization' => 'token '.getenv('GITHUB_TOKEN'),
+])->put($res->json()['url'].'/topics', [
     'names' => ['laravel', 'oauth', 'socialite', 'oauth1', 'oauth2', 'socialite-providers', 'social-media'],
 ]);
 
