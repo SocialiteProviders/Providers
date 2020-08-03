4c1dbe862a3c4fe7baa4a22d17d868668c491d09
atymic <atymicq@gmail.com>
2020-08-03 03:00:43 +0000

    Apply fixes from StyleCI

--- tools/update-repo-descriptions.php
+++ tools/update-repo-descriptions.php
@@ -4,8 +4,7 @@
  * Automatically update all of the repos to have a consistent description/URL and point people to the correct
  * documentation.
  */
-
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
 $excludedRepos = [
     'Providers',
@@ -17,7 +16,7 @@ $excludedRepos = [
 
 $repos = collect(range(1, 5))
     ->map(function (int $page) {
-        return \Zttp\Zttp::withHeaders(['Accept' => 'application/vnd.github.v3+json'])->get('https://api.github.com/orgs/SocialiteProviders/repos?per_page=100&page=' . $page)->json();
+        return \Zttp\Zttp::withHeaders(['Accept' => 'application/vnd.github.v3+json'])->get('https://api.github.com/orgs/SocialiteProviders/repos?per_page=100&page='.$page)->json();
     })
     ->flatten(1)
     ->filter(function (array $repo) use ($excludedRepos) {
@@ -25,22 +24,22 @@ $repos = collect(range(1, 5))
     })
     ->each(function (array $repo) {
         $res = \Zttp\Zttp::withHeaders([
-            'Accept' => 'application/vnd.github.v3+json',
-            'Authorization' => 'token ' . getenv('GITHUB_TOKEN'),
+            'Accept'        => 'application/vnd.github.v3+json',
+            'Authorization' => 'token '.getenv('GITHUB_TOKEN'),
         ])
             ->patch($repo['url'], [
                 'description' => sprintf('[READ ONLY] Subtree split of the SocialiteProviders/%s Provider (see SocialiteProviders/Providers)', $repo['name']),
-                'homepage' => sprintf('https://socialiteproviders.com/%s/', $repo['name']),
-                'has_issues' => false,
+                'homepage'    => sprintf('https://socialiteproviders.com/%s/', $repo['name']),
+                'has_issues'  => false,
             ]);
 
         echo sprintf("Updated Repo: %s, response code: %s\n", $repo['name'], $res->status());
 
         $res = \Zttp\Zttp::withHeaders([
-            'Accept' => 'application/vnd.github.mercy-preview+json',
-            'Authorization' => 'token ' . getenv('GITHUB_TOKEN'),
+            'Accept'        => 'application/vnd.github.mercy-preview+json',
+            'Authorization' => 'token '.getenv('GITHUB_TOKEN'),
         ])
-            ->put($repo['url'] . '/topics', [
+            ->put($repo['url'].'/topics', [
                 'names' => ['laravel', 'oauth', 'socialite', 'oauth1', 'oauth2', 'socialite-providers', 'social-media'],
             ]);
 
