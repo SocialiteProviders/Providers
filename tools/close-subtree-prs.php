173317c4be28c1e36157bb9ebe1bc6cc1efb3df5
atymic <atymicq@gmail.com>
2020-10-11 10:55:26 +1100

    fix: style fixes

--- tools/close-subtree-prs.php
+++ tools/close-subtree-prs.php
@@ -4,12 +4,11 @@
  * Automatically update all of the repos to have a consistent description/URL and point people to the correct
  * documentation.
  */
-require_once __DIR__ . '/../vendor/autoload.php';
-
+require_once __DIR__.'/../vendor/autoload.php';
 
 $repos = collect(range(1, 1))
     ->map(function (int $page) {
-        return \Zttp\Zttp::withHeaders(['Accept' => 'application/vnd.github.v3+json'])->get('https://api.github.com/orgs/SocialiteProviders/repos?per_page=100&page=' . $page)->json();
+        return \Zttp\Zttp::withHeaders(['Accept' => 'application/vnd.github.v3+json'])->get('https://api.github.com/orgs/SocialiteProviders/repos?per_page=100&page='.$page)->json();
     })
     ->flatten(1)
     ->filter(function (array $repo) {
@@ -17,8 +16,8 @@ $repos = collect(range(1, 1))
     })
     ->each(function (array $repo) {
         $res = \Zttp\Zttp::withHeaders([
-            'Accept' => 'application/vnd.github.v3+json',
-            'Authorization' => 'token ' . getenv('GITHUB_TOKEN'),
+            'Accept'        => 'application/vnd.github.v3+json',
+            'Authorization' => 'token '.getenv('GITHUB_TOKEN'),
         ])->get(sprintf('https://api.github.com/repos/SocialiteProviders/%s/pulls?state=open', $repo['name']));
 
         $prs = collect($res->json());
@@ -26,21 +25,20 @@ $repos = collect(range(1, 1))
         echo sprintf("Found Repo: %s, %d open PRs\n", $repo['name'], $prs->count());
 
         if ($prs->isEmpty()) {
-
             return;
         }
 
         $prs->map(function (array $pr) {
             $res = \Zttp\Zttp::withHeaders([
-                'Accept' => 'application/vnd.github.v3+json',
-                'Authorization' => 'token ' . getenv('GITHUB_TOKEN'),
+                'Accept'        => 'application/vnd.github.v3+json',
+                'Authorization' => 'token '.getenv('GITHUB_TOKEN'),
             ])->patch($pr['url'], [
                 'state' => 'closed',
             ]);
 
             $res = \Zttp\Zttp::withHeaders([
-                'Accept' => 'application/vnd.github.v3+json',
-                'Authorization' => 'token ' . getenv('GITHUB_TOKEN'),
+                'Accept'        => 'application/vnd.github.v3+json',
+                'Authorization' => 'token '.getenv('GITHUB_TOKEN'),
             ])->post($pr['comments_url'], [
                 'body' => "This repository is a **READ ONLY** subtree split from [SocialiteProviders/Providers](https://github.com/SocialiteProviders/Providers).\n\nPlease open a PR against [SocialiteProviders/Providers](https://github.com/SocialiteProviders/Providers). ",
             ]);
