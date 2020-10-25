b5491f3e06525ec45f7a647e5bcd660028abede0
atymic <atymicq@gmail.com>
2020-10-26 10:59:59 +1100

    chore: style fixes
    Co-authored-by: atymic <atymic@users.noreply.github.com>

--- src/Gumroad/Provider.php
+++ src/Gumroad/Provider.php
@@ -68,7 +68,7 @@ class Provider extends AbstractProvider
     protected function getTokenFields($code)
     {
         return array_merge(parent::getTokenFields($code), [
-            'grant_type' => 'authorization_code'
+            'grant_type' => 'authorization_code',
         ]);
     }
 }
