298efc020826c9889eef50178be27ba5eb03556d
Mariano Moreyra <mmoreyra@gmail.com>
2020-10-28 18:30:22 -0300

    Add MercadoLibre provider

---
+++ src/MercadoLibre/MercadoLibreExtendSocialite.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SocialiteProviders\MercadoLibre;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class MercadoLibreExtendSocialite
+{
+    /**
+     * Register the provider.
+     *
+     * @param \SocialiteProviders\Manager\SocialiteWasCalled $socialiteWasCalled
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite('mercadolibre', Provider::class);
+    }
+}
