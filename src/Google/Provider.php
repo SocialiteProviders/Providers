2d89fbe1f89c51d3e674d7f923b91d3943c435f3
Lucas Michot <lucas.michot@shopify.com>
2021-04-13 10:24:51 +0200

    Simplify Google provider.

--- src/Google/Provider.php
+++ src/Google/Provider.php
@@ -2,88 +2,16 @@
 
 namespace SocialiteProviders\Google;
 
-use Illuminate\Support\Arr;
-use SocialiteProviders\Manager\OAuth2\AbstractProvider;
-use SocialiteProviders\Manager\OAuth2\User;
+use Laravel\Socialite\Two\GoogleProvider;
+use SocialiteProviders\Manager\ConfigTrait;
+use SocialiteProviders\Manager\Contracts\OAuth2\ProviderInterface;
 
-class Provider extends AbstractProvider
+class Provider extends GoogleProvider implements ProviderInterface
 {
+    use ConfigTrait;
+
     /**
      * Unique Provider Identifier.
      */
     public const IDENTIFIER = 'GOOGLE';
-
-    /**
-     * {@inheritdoc}
-     */
-    protected $scopes = [
-        'openid',
-        'profile',
-        'email',
-    ];
-
-    /**
-     * {@inheritdoc}
-     */
-    protected $scopeSeparator = ' ';
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function getAuthUrl($state)
-    {
-        return $this->buildAuthUrlFromBase(
-            'https://accounts.google.com/o/oauth2/auth',
-            $state
-        );
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function getTokenUrl()
-    {
-        return 'https://oauth2.googleapis.com/token';
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function getUserByToken($token)
-    {
-        $response = $this->getHttpClient()->get(
-            'https://www.googleapis.com/oauth2/v3/userinfo',
-            [
-                'headers' => [
-                    'Authorization' => 'Bearer '.$token,
-                ],
-            ]
-        );
-
-        return json_decode($response->getBody()->getContents(), true);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function mapUserToObject(array $user)
-    {
-        return (new User())->setRaw($user)->map([
-            'id'       => $user['sub'],
-            'nickname' => Arr::get($user, 'name'),
-            'name'     => $user['name'],
-            'email'    => $user['email'],
-            'avatar'   => $user['picture'],
-        ]);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function getTokenFields($code)
-    {
-        return array_merge(parent::getTokenFields($code), [
-            'grant_type' => 'authorization_code',
-        ]);
-    }
 }
