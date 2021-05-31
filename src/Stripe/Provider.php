2421c53305757e9a07d5d581d92c1ad446418337
Francis John <francis.john@colorado.edu>
2021-05-30 20:48:56 -0700

    Fix Stripe "email" error
    Fixes https://github.com/SocialiteProviders/Providers/issues/681 - missing "email" in account API response.

--- src/Stripe/Provider.php
+++ src/Stripe/Provider.php
@@ -64,10 +64,15 @@ class Provider extends AbstractProvider
         } elseif (isset($user['display_name'])) { // original location
             $nickname = $user['display_name'];
         }
+        
+        $email = null;
+        if (isset($user['email'])) {
+            $email = $user['email'];
+        }
 
         return (new User())->setRaw($user)->map([
             'id'   => $user['id'], 'nickname' => $nickname,
-            'name' => null, 'email' => $user['email'], 'avatar' => null,
+            'name' => null, 'email' => $email, 'avatar' => null,
         ]);
     }
 
