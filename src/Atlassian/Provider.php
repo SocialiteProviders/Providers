2bd0a3fb4563baff1244d65a0b70cd9401bf1932
Ryan Cooley <ryancooley@users.noreply.github.com>
2021-06-06 15:39:03 -0700

    Create Atlassian provider (#623)
    Co-authored-by: atymic <atymicq@gmail.com>

---
+++ src/Atlassian/Provider.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace SocialiteProviders\Atlassian;
+
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider
+{
+    /**
+     * Unique Provider Identifier.
+     */
+    const IDENTIFIER = 'ATLASSIAN';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $parameters = [
+        'prompt'   => 'consent',
+        'audience' => 'api.atlassian.com',
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopes = ['read:me'];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase('https://auth.atlassian.com/authorize', $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://auth.atlassian.com/oauth/token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get('https://api.atlassian.com/me', [
+            'headers' => [
+                'Authorization' => 'Bearer '.$token,
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id'       => $user['account_id'],
+            'nickname' => $user['email'],
+            'name'     => $user['name'],
+            'email'    => $user['email'],
+            'avatar'   => $user['picture'],
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code',
+        ]);
+    }
+}
