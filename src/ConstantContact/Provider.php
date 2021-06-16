05eb9a965be2023405461ac778267f57b9aad801
jack <jackstowey@gmail.com>
2021-06-16 12:21:04 +0100

    Updated to use V3 endpoints and scopes

--- src/ConstantContact/Provider.php
+++ src/ConstantContact/Provider.php
@@ -15,10 +15,15 @@ class Provider extends AbstractProvider
     /**
      * {@inheritdoc}
      */
+    protected $scopeSeparator = '+';
+
+    /**
+     * {@inheritdoc}
+     */
     protected function getAuthUrl($state)
     {
         return $this->buildAuthUrlFromBase(
-            'https://oauth2.constantcontact.com/oauth2/oauth/siteowner/authorize',
+            'https://api.cc.email/v3/idfed',
             $state
         );
     }
@@ -28,7 +33,7 @@ class Provider extends AbstractProvider
      */
     protected function getTokenUrl()
     {
-        return 'https://oauth2.constantcontact.com/oauth2/oauth/token';
+        return 'https://idfed.constantcontact.com/as/token.oauth2';
     }
 
     /**
@@ -37,7 +42,7 @@ class Provider extends AbstractProvider
     protected function getUserByToken($token)
     {
         $response = $this->getHttpClient()->get(
-            'https://api.constantcontact.com/v2/account/info?api_key='.$this->clientId,
+            'https://api.cc.email/v3/account/summary',
             [
                 'headers' => [
                     'Authorization' => 'Bearer '.$token,
@@ -54,9 +59,9 @@ class Provider extends AbstractProvider
     protected function mapUserToObject(array $user)
     {
         return (new User())->setRaw($user)->map([
-            'id'    => null, 'nickname' => null,
+            'id'    => $user['encoded_account_id'], 'nickname' => null,
             'name'  => $user['first_name'].' '.$user['last_name'],
-            'email' => $user['email'], 'avatar' => null,
+            'email' => $user['contact_email'], 'avatar' => null,
         ]);
     }
 
