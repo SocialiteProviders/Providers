9ca50d1f47b7497c087132aa4a8d73ae40b3fb7c
nunar <anze@nunar.si>
2021-07-08 09:21:22 +0200

    Fixed undefined field for Rekono (#697)

--- src/Rekono/Provider.php
+++ src/Rekono/Provider.php
@@ -69,7 +69,7 @@ class Provider extends AbstractProvider
     {
         return (new User())->setRaw($user)->map([
             'id'    => $user['sub'],
-            'name'  => $user['name'],
+            'name'  => $user['name'] ?? null,
             'email' => $user['email'],
         ]);
     }
