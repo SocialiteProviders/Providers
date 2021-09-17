3d8a02b7686030f6682c15ea84aab50461b4762f
Colin Fox <tethlah@users.noreply.github.com>
2021-09-17 11:30:25 -0500

    Added FDev
    Added FDev Provider for FrontierDevelopment

---
+++ src/FDev/FDevExtendSocialite.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SocialiteProviders\FDev;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class FDevExtendSocialite
+{
+    /**
+     * Register the provider.
+     *
+     * @param  \SocialiteProviders\Manager\SocialiteWasCalled $socialiteWasCalled
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite('fdev', Provider::class);
+    }
+}
---
+++ src/FDev/Provider.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace SocialiteProviders\FDev;
+
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider
+{
+    /**
+     * Unique Provider Identifier.
+     */
+    const IDENTIFIER = 'FDEV';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopes = ['auth capi'];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase('https://auth.frontierstore.net/auth', $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://auth.frontierstore.net/token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get('https://auth.frontierstore.net/decode', [
+            'headers' => [
+                'Authorization' => 'Bearer '.$token,
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id'       => $user['id'],
+            'nickname' => $user['username'],
+            'name'     => $user['name'],
+            'email'    => $user['email'],
+            'avatar'   => $user['avatar'],
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code'
+        ]);
+    }
+}
