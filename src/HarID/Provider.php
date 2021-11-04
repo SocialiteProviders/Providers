c63cd4f0b69aac88e4adfcb381f67631e18693c8
Pjotr Savitski <pjotr.savitski@gmail.com>
2021-11-04 06:45:41 +0200

    HarID (#758)
    Co-authored-by: Lucas Michot <lucas@semalead.com>

---
+++ src/HarID/Provider.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace SocialiteProviders\HarID;
+
+use GuzzleHttp\RequestOptions;
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider
+{
+    /**
+     * Unique Provider Identifier.
+     */
+    public const IDENTIFIER = 'HARID';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopes = [
+        'openid',
+        'profile',
+        'email',
+        'session_type',
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopeSeparator = ' ';
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function additionalConfigKeys()
+    {
+        return ['use_test_idp'];
+    }
+
+    /**
+     * Returns base URL for idP endpoints with check for test or live environment.
+     *
+     * @return string
+     */
+    protected function getEndpointBaseUrl()
+    {
+        return (bool) $this->getConfig('use_test_idp', false) === true ? 'https://test.harid.ee/et' : 'https://harid.ee/et';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase($this->getEndpointBaseUrl().'/authorizations/new', $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return $this->getEndpointBaseUrl().'/access_tokens';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get($this->getEndpointBaseUrl().'/user_info', [
+            RequestOptions::QUERY => [
+                'prettyPrint' => 'false',
+            ],
+            RequestOptions::HEADERS => [
+                'Accept'        => 'application/json',
+                'Authorization' => 'Bearer '.$token,
+            ],
+        ]);
+
+        return json_decode((string) $response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id'       => $user['sub'],
+            'nickname' => $user['sub'],
+            'name'     => $user['name'],
+            'email'    => $user['email'],
+            'avatar'   => '',
+        ]);
+    }
+}
