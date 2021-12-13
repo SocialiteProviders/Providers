f71795413ca1914d8ac11244644841646ef95fc3
Andrzej Kupczyk <kontakt@andrzejkupczyk.pl>
2021-12-13 01:31:27 +0100

    Add MusicBrainz Provider (#768)
    Co-authored-by: Lucas Michot <lucas@semalead.com>

---
+++ src/MusicBrainz/Provider.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace SocialiteProviders\MusicBrainz;
+
+use GuzzleHttp\RequestOptions;
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider
+{
+    /**
+     * Unique Provider Identifier.
+     */
+    public const IDENTIFIER = 'MUSICBRAINZ';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopes = ['profile'];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopeSeparator = ' ';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $usesPKCE = true;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase(
+            'https://musicbrainz.org/oauth2/authorize',
+            $state
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://musicbrainz.org/oauth2/token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get(
+            'https://musicbrainz.org/oauth2/userinfo',
+            [
+                RequestOptions::HEADERS => [
+                    'Authorization' => "Bearer $token",
+                ],
+            ]
+        );
+
+        return json_decode((string) $response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id'   => $user['metabrainz_user_id'],
+            'name' => $user['sub'],
+        ]);
+    }
+}
---
+++ src/MusicBrainz/README.md
@@ -0,0 +1,42 @@
+# MusicBrainz
+
+```bash
+composer require socialiteproviders/musicbrainz
+```
+
+## Installation & Basic Usage
+
+Please see the [Base Installation Guide](https://socialiteproviders.com/usage/), then follow the provider specific instructions below.
+
+### Add configuration to `config/services.php`
+
+```php
+'musicbrainz' => [
+  'client_id' => env('MUSICBRAINZ_CLIENT_ID'),
+  'client_secret' => env('MUSICBRAINZ_CLIENT_SECRET'),
+  'redirect' => env('MUSICBRAINZ_REDIRECT_URI')
+],
+```
+
+### Add provider event listener
+
+Configure the package's listener to listen for `SocialiteWasCalled` events.
+
+Add the event to your `listen[]` array in `app/Providers/EventServiceProvider`. See the [Base Installation Guide](https://socialiteproviders.com/usage/) for detailed instructions.
+
+```php
+protected $listen = [
+    \SocialiteProviders\Manager\SocialiteWasCalled::class => [
+        // ... other providers
+        \SocialiteProviders\MusicBrainz\MusicBrainzExtendSocialite::class.'@handle',
+    ],
+];
+```
+
+### Usage
+
+You should now be able to use the provider like you would regularly use Socialite (assuming you have the facade installed):
+
+```php
+return Socialite::driver('musicbrainz')->redirect();
+```
