51ae3cdad3d4f423351a23425920638dc2bafde0
macaron <19354702+macaron@users.noreply.github.com>
2021-12-15 12:04:06 +0900

    Add TwitCasting Provider

---
+++ src/TwitCasting/Provider.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace SocialiteProviders\TwitCasting;
+
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider
+{
+    /**
+     * Unique Provider Identifier.
+     */
+    const IDENTIFIER = 'TWITCASTING';
+
+    const BASE_URL = 'https://apiv2.twitcasting.tv';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopes = [''];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase(self::BASE_URL . '/oauth2/authorize', $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return self::BASE_URL . '/oauth2/access_token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get(self::BASE_URL . '/verify_credentials', [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $token,
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id' => $user['user']['id'],
+            'nickname' => $user['user']['screen_id'],
+            'name' => $user['user']['name'],
+            'email' => null,
+            'avatar' => $user['user']['image'],
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code'
+        ]);
+    }
+}
---
+++ src/TwitCasting/README.md
@@ -0,0 +1,57 @@
+# TwitCasting
+
+```bash
+composer require socialiteproviders/twitcasting
+```
+
+## Register an application
+
+Add new application at [https://en.twitcasting.tv/indexapi.php](https://en.twitcasting.tv/indexapi.php).
+
+## Installation & Basic Usage
+
+Please see the [Base Installation Guide](https://socialiteproviders.com/usage/), then follow the provider specific instructions below.
+
+### Add configuration to `config/services.php`
+
+```php
+'twitcasting' => [
+  'client_id' => env('TWITCASTING_CLIENT_ID'),
+  'client_secret' => env('TWITCASTING_CLIENT_SECRET'),
+  'redirect' => env('TWITCASTING_REDIRECT_URI')
+],
+```
+
+### Add provider event listener
+
+Configure the package's listener to listen for `SocialiteWasCalled` events.
+
+Add the event to your `listen[]` array in `app/Providers/EventServiceProvider`. See the [Base Installation Guide](https://socialiteproviders.com/usage/) for detailed instructions.
+
+```php
+protected $listen = [
+    \SocialiteProviders\Manager\SocialiteWasCalled::class => [
+        // ... other providers
+        'SocialiteProviders\\TwitCasting\\TwitCastingExtendSocialite@handle',
+    ],
+];
+```
+
+### Usage
+
+You should now be able to use the provider like you would regularly use Socialite (assuming you have the facade installed):
+
+```php
+return Socialite::driver('twitcasting')->redirect();
+```
+
+### Returned User fields
+
+- ``id``
+- ``nickname``
+- ``name``
+- ``avatar``
+
+### Reference
+
+- [TwitCasting API Reference](https://apiv2-doc.twitcasting.tv)
