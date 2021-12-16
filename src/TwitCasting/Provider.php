32fa1856d9122e4e63fa9f0d5d1fbd0ce1153b5b
Lucas Michot <lucas@semalead.com>
2021-12-16 09:10:44 +0100

    Some code adjustments.

--- src/TwitCasting/Provider.php
+++ src/TwitCasting/Provider.php
@@ -2,6 +2,7 @@
 
 namespace SocialiteProviders\TwitCasting;
 
+use GuzzleHttp\RequestOptions;
 use SocialiteProviders\Manager\OAuth2\AbstractProvider;
 use SocialiteProviders\Manager\OAuth2\User;
 
@@ -10,9 +11,12 @@ class Provider extends AbstractProvider
     /**
      * Unique Provider Identifier.
      */
-    const IDENTIFIER = 'TWITCASTING';
+    public const IDENTIFIER = 'TWITCASTING';
 
-    const BASE_URL = 'https://apiv2.twitcasting.tv';
+    /**
+     * The Base URL.
+     */
+    public const BASE_URL = 'https://apiv2.twitcasting.tv';
 
     /**
      * {@inheritdoc}
@@ -41,12 +45,12 @@ class Provider extends AbstractProvider
     protected function getUserByToken($token)
     {
         $response = $this->getHttpClient()->get(self::BASE_URL.'/verify_credentials', [
-            'headers' => [
+            RequestOptions::HEADERS => [
                 'Authorization' => 'Bearer '.$token,
             ],
         ]);
 
-        return json_decode($response->getBody(), true);
+        return json_decode((string) $response->getBody(), true);
     }
 
     /**
@@ -60,16 +64,6 @@ class Provider extends AbstractProvider
             'name'     => $user['user']['name'],
             'email'    => null,
             'avatar'   => $user['user']['image'],
-        ]);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function getTokenFields($code)
-    {
-        return array_merge(parent::getTokenFields($code), [
-            'grant_type' => 'authorization_code',
         ]);
     }
 }
