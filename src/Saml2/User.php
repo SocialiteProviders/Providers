a70d30bea0ced3f1b63152947a7b0607860be6b7
Dániel Zoltán Tolnai <daniel.tolnai@apicore.de>
2021-12-21 09:05:09 +0000

    Add default mapped attributes to the User model

--- src/Saml2/User.php
+++ src/Saml2/User.php
@@ -9,6 +9,27 @@ class User extends AbstractUser
 {
     protected $assertion;
 
+    /**
+     * The user's first name.
+     *
+     * @var string|null
+     */
+    public $first_name;
+
+    /**
+     * The user's last name.
+     *
+     * @var string|null
+     */
+    public $last_name;
+
+    /**
+     * The user's UPN (user principal name).
+     *
+     * @var string|null
+     */
+    public $upn;
+
     public function getAssertion(): Assertion
     {
         return $this->assertion;
@@ -19,5 +40,35 @@ class User extends AbstractUser
         $this->assertion = $assertion;
 
         return $this;
+    }
+
+    /**
+     * Get the first name of the user.
+     *
+     * @return string|null
+     */
+    public function getFirstName()
+    {
+        return $this->first_name;
+    }
+
+    /**
+     * Get the last name of the user.
+     *
+     * @return string|null
+     */
+    public function getLastName()
+    {
+        return $this->last_name;
+    }
+
+    /**
+     * Get the UPN of the user.
+     *
+     * @return string|null
+     */
+    public function getUpn()
+    {
+        return $this->upn;
     }
 }
