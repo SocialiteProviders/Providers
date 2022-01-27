e193559bc7d0f6834605b65dc04da9dcdf0e457d
Omar Vega Ramos <ovruni@riseup.net>
2022-01-27 18:58:54 -0500

    Add OpenStreetMap Provider (#777)
    Co-authored-by: Omar Vega Ramos <ovruni@gnu.org.pe>
    Co-authored-by: atymic <atymicq@gmail.com>

---
+++ src/OpenStreetMap/Provider.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace SocialiteProviders\OpenStreetMap;
+
+use GuzzleHttp\RequestOptions;
+use Illuminate\Support\Arr;
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider
+{
+    /**
+     * Unique Provider Identifier.
+     */
+    public const IDENTIFIER = 'OPENSTREETMAP';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopes = ['read_prefs'];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopeSeparator = ' ';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase('https://www.openstreetmap.org/oauth2/authorize', $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://www.openstreetmap.org/oauth2/token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get(
+            'https://api.openstreetmap.org/api/0.6/user/details.json',
+            [
+                RequestOptions::HEADERS => [
+                    'Authorization' => 'Bearer '.$token,
+                ],
+            ]
+        );
+
+        $json = json_decode($response->getBody(), true);
+
+        return $json['user'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id'       => $user['id'],
+            'nickname' => $user['display_name'],
+            'name'     => $user['display_name'],
+            'email'    => null,
+            'avatar'   => Arr::get($user, 'img.href', null),
+        ]);
+    }
+}
