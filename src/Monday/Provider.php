675ce65ba10795a2da4e3272adb17f43731f1642
Amir Rami <amirrami.ce@gmail.com>
2022-01-28 01:26:25 +0100

    Add Monday provider (#779)
    Co-authored-by: Amir <a.rami@adaptech.me>
    Co-authored-by: atymic <atymicq@gmail.com>

---
+++ src/Monday/Provider.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace SocialiteProviders\Monday;
+
+use GuzzleHttp\RequestOptions;
+use Laravel\Socialite\Two\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider
+{
+    /**
+     * Unique Provider Identifier.
+     */
+    public const IDENTIFIER = 'MONDAY';
+
+    /**
+     * The scopes being requested.
+     *
+     * @var array
+     */
+    protected $scopes = ['me:read'];
+
+    /**
+     * The separating character for the requested scopes.
+     *
+     * @var string
+     */
+    protected $scopeSeparator = ' ';
+
+    /**
+     * @inheritDoc
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase('https://auth.monday.com/oauth2/authorize', $state);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://auth.monday.com/oauth2/token';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->httpClient
+            ->post('https://api.monday.com/v2', [
+                RequestOptions::HEADERS => [
+                    'Content-Type'  => 'application/json',
+                    'Authorization' => $token,
+                ],
+                RequestOptions::BODY => json_encode([
+                    'query' => <<<'GQL'
+                        query {
+                            me {
+                                birthday
+                                country_code
+                                created_at
+                                join_date
+                                email
+                                enabled
+                                id
+                                is_admin
+                                is_guest
+                                is_pending
+                                is_view_only
+                                location
+                                mobile_phone
+                                name
+                                phone
+                                photo_original
+                                photo_small
+                                photo_thumb
+                                photo_thumb_small
+                                photo_tiny
+                                teams {
+                                    id
+                                    name
+                                    picture_url
+                                }
+                                time_zone_identifier
+                                title
+                                url
+                                utc_hours_diff
+                            }
+                        }
+GQL
+                ]),
+            ]);
+
+        return json_decode($response->getBody()->getContents(), true)['data']['me'];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id'     => $user['id'],
+            'name'   => $user['name'],
+            'email'  => $user['email'],
+            'avatar' => $user['photo_original'],
+        ]);
+    }
+}
