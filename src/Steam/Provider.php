8468ccff3e61dfabd34ebcf7911c50c94b1ab48b
Lucas Michot <lucas@semalead.com>
2022-03-16 12:18:21 +0100

    Code adjustment.

--- src/Steam/Provider.php
+++ src/Steam/Provider.php
@@ -162,6 +162,7 @@ class Provider extends AbstractProvider
      * Checks the steam login.
      *
      * @return bool
+     * @throws \SocialiteProviders\Steam\OpenIDValidationException
      */
     public function validate()
     {
@@ -321,12 +322,6 @@ class Provider extends AbstractProvider
     {
         $allowedHosts = $this->getConfig('allowed_hosts', []);
 
-        if (empty($allowedHosts)) {
-            return true;
-        }
-
-        $urlParse = parse_url($url, PHP_URL_HOST);
-
-        return in_array($urlParse, $allowedHosts);
+        return count($allowedHosts) === 0 || in_array(parse_url($url, PHP_URL_HOST), $allowedHosts, true);
     }
 }
