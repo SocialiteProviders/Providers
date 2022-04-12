3e65167e7c804f7cc0a9daef834d7ba506b47ed7
lulu <sarah.j.ting@gmail.com>
2022-04-12 21:26:17 +0900

    add Toyhouse provider

---
+++ src/Toyhouse/Provider.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace SocialiteProviders\Toyhouse;
+
+use GuzzleHttp\RequestOptions;
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider
+{
+    /**
+     * Unique Provider Identifier.
+     */
+    public const IDENTIFIER = 'TOYHOUSE';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopes = [''];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase('https://toyhou.se/~oauth/authorize', $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://toyhou.se/~oauth/token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get('https://toyhou.se/~api/v1/me', [
+            RequestOptions::HEADERS => [
+                'Authorization' => sprintf('Bearer %s', $token),
+            ],
+        ]);
+
+        return json_decode((string) $response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id'       => $user['id'],
+            'email'    => $user['email'] ?? null,
+            'nickname' => $user['username'],
+            'name'     => $user['username'],
+            'avatar'   => $user['avatar'],
+        ]);
+    }
+}
