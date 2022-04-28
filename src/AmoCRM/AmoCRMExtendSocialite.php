7cfda5da69e0669e6ff7f5267fb4a37f7e59d22e
Vladimir Mityukov <mityukov@gmail.com>
2022-04-28 11:46:53 +0400

    add amoCRM provider

---
+++ src/AmoCRM/AmoCRMExtendSocialite.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SocialiteProviders\AmoCRM;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class AmoCRMExtendSocialite
+{
+    /**
+     * Register the provider.
+     *
+     * @param \SocialiteProviders\Manager\SocialiteWasCalled $socialiteWasCalled
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite('amocrm', Provider::class);
+    }
+}
---
+++ src/AmoCRM/Provider.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace SocialiteProviders\AmoCRM;
+
+use GuzzleHttp\RequestOptions;
+use SocialiteProviders\Manager\OAuth2\User;
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+
+class Provider extends AbstractProvider
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase('https://www.amocrm.ru/oauth', $state);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function getTokenUrl()
+    {
+        $domain = request('referer');
+
+        return "https://{$domain}/oauth2/access_token";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $domain = request('referer');
+
+        $response = $this->getHttpClient()->get("https://{$domain}/api/v4/account", [
+            RequestOptions::HEADERS => [
+                'Authorization' => 'Bearer ' . $token,
+            ],
+        ]);
+
+        $account = json_decode((string) $response->getBody(), true);
+        $response = $this->getHttpClient()->get("https://{$domain}/api/v4/users/{$account['current_user_id']}", [
+            RequestOptions::HEADERS => [
+                'Authorization' => 'Bearer ' . $token,
+            ],
+        ]);
+
+        return json_decode((string) $response->getBody(), true);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id' => $user['id'],
+            'name' => $user['name'],
+            'nickname' => $user['email'],
+            'email' => $user['email'],
+        ]);
+    }
+}
