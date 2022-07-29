6bb37018b56b70604045b9bc786e8d82c63b159e
Richard Franks <git@rf152.co.uk>
2022-07-29 16:57:10 +0100

    styleci

--- src/Authentik/Provider.php
+++ src/Authentik/Provider.php
@@ -66,15 +66,15 @@ class Provider extends AbstractProvider
     protected function mapUserToObject(array $user)
     {
         return (new User())->setRaw($user)->map([
-            'email'              => $user['email'] ?? "",
+            'email'              => $user['email'] ?? '',
             'email_verified'     => $user['email_verified'] ?? null,
-            'name'               => $user['name'] ?? "",
-            'given_name'         => $user['given_name'] ?? "",
-            'family_name'        => $user['family_name'] ?? "",
-            'preferred_username' => $user['preferred_username'] ?? "",
-            'nickname'           => $user['nickname'] ?? "",
-            'groups'             => $user['groups'] ?? "",
-            'sub'                => $user['sub'] ?? "",
+            'name'               => $user['name'] ?? '',
+            'given_name'         => $user['given_name'] ?? '',
+            'family_name'        => $user['family_name'] ?? '',
+            'preferred_username' => $user['preferred_username'] ?? '',
+            'nickname'           => $user['nickname'] ?? '',
+            'groups'             => $user['groups'] ?? '',
+            'sub'                => $user['sub'] ?? '',
         ]);
     }
 
