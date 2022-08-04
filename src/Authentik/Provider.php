9244842bff1b48c6888d02e02e1088e1cb0d0ef6
Richard Franks <git@rf152.co.uk>
2022-08-04 09:06:47 +0100

    Added Socialite provider for Authentik (#869)
    Co-authored-by: atymic <atymicq@gmail.com>

---
+++ src/Authentik/Provider.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace SocialiteProviders\Authentik;
+
+use GuzzleHttp\RequestOptions;
+use InvalidArgumentException;
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider
+{
+    /**
+     * Unique Provider Identifier.
+     */
+    public const IDENTIFIER = 'AUTHENTIK';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopes = ['openid goauthentik.io/api profile email'];
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function additionalConfigKeys()
+    {
+        return ['base_url'];
+    }
+
+    protected function getBaseUrl()
+    {
+        $baseurl = $this->getConfig('base_url');
+        if ($baseurl === null) {
+            throw new InvalidArgumentException('Missing base_url');
+        }
+
+        return rtrim($baseurl, '/');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase($this->getBaseUrl().'/application/o/authorize/', $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return $this->getBaseUrl().'/application/o/token/';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get(
+            $this->getBaseUrl().'/application/o/userinfo/',
+            [
+                RequestOptions::HEADERS => [
+                    'Authorization' => 'Bearer '.$token,
+                ],
+            ]
+        );
+
+        return json_decode((string) $response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'email'              => $user['email'] ?? null,
+            'email_verified'     => $user['email_verified'] ?? null,
+            'name'               => $user['name'] ?? null,
+            'given_name'         => $user['given_name'] ?? null,
+            'family_name'        => $user['family_name'] ?? null,
+            'preferred_username' => $user['preferred_username'] ?? null,
+            'nickname'           => $user['nickname'] ?? null,
+            'groups'             => $user['groups'] ?? null,
+            'id'                 => $user['sub'],
+        ]);
+    }
+}
---
+++ src/Authentik/README.md
@@ -0,0 +1,61 @@
+# Authentik
+
+```bash
+composer require socialiteproviders/authentik
+```
+
+## Installation & Basic Usage
+
+Please see the [Base Installation Guide](https://socialiteproviders.com/usage/),
+then follow the provider specific instructions below.
+
+### Prepare OAuth provider & application in Authentik
+
+Create a new OAuth provider within Authentik, according to the Authentik
+Documentation (https://goauthentik.io/docs/providers/oauth2/)
+
+### Add configuration to `config/services.php`
+
+```php
+'authentik' => [
+  'base_url' => env('AUTHENTIK_BASE_URL'),
+  'client_id' => env('AUTHENTIK_CLIENT_ID'),
+  'client_secret' => env('AUTHENTIK_CLIENT_SECRET'),
+  'redirect' => env('AUTHENTIK_REDIRECT_URI')
+],
+```
+
+### Add provider event listener
+
+Configure the package's listener to listen for `SocialiteWasCalled` events.
+
+Add the event to your `listen[]` array in `app/Providers/EventServiceProvider`.
+See the [Base Installation Guide](https://socialiteproviders.com/usage/) for
+detailed instructions.
+
+```php
+protected $listen = [
+    \SocialiteProviders\Manager\SocialiteWasCalled::class => [
+        // ... other providers
+        'SocialiteProviders\\Authentik\\AuthentikExtendSocialite@handle',
+    ],
+];
+```
+
+### Usage
+
+You should now be able to use the provider like you would regularly use
+Socialite (assuming you have the facade installed):
+
+```php
+return Socialite::driver('authentik')->redirect();
+```
+
+To redirect to the authentication, and then:
+
+```php
+$user = Socialite::driver('authentik')->user()
+```
+
+In the return function. The user will contain a `name` and `email` field
+populated from the OAuth source.
