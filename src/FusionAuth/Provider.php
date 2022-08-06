7d69d57855eaa9abb4d8d08ae9fc5f6ef93617be
Danilo Polani <danilo.polani@gmail.com>
2022-08-06 11:12:53 +0200

    Add FusionAuth provider (#877)
    Co-authored-by: Lucas Michot <lucas@semalead.com>
    Co-authored-by: atymic <atymicq@gmail.com>

---
+++ src/FusionAuth/Provider.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace SocialiteProviders\FusionAuth;
+
+use GuzzleHttp\RequestOptions;
+use InvalidArgumentException;
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider
+{
+    public const IDENTIFIER = 'FUSIONAUTH';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopes = [
+        'email',
+        'openid',
+        'profile',
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopeSeparator = ' ';
+
+    /**
+     * Get the base URL.
+     *
+     * @throws \InvalidArgumentException
+     *
+     * @return string
+     */
+    protected function getFusionAuthUrl()
+    {
+        $baseUrl = $this->getConfig('base_url');
+
+        if ($baseUrl === null) {
+            throw new InvalidArgumentException('Missing base_url');
+        }
+
+        return rtrim($baseUrl).'/oauth2';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function additionalConfigKeys()
+    {
+        return [
+            'base_url',
+            'tenant_id',
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase($this->getFusionAuthUrl().'/authorize', $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return $this->getFusionAuthUrl().'/token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get($this->getFusionAuthUrl().'/userinfo', [
+            RequestOptions::HEADERS => [
+                'Authorization' => 'Bearer '.$token,
+            ],
+        ]);
+
+        return json_decode((string) $response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id'       => $user['sub'],
+            'nickname' => $user['preferred_username'] ?? null,
+            'name'     => $user['name'],
+            'email'    => $user['email'],
+            'avatar'   => $user['picture'],
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @throws \InvalidArgumentException
+     */
+    protected function getCodeFields($state = null)
+    {
+        $tenantId = $this->getConfig('tenant_id');
+
+        if ($tenantId === null) {
+            throw new InvalidArgumentException('Missing tenant_id');
+        }
+
+        return array_merge(parent::getCodeFields($state), [
+            'tenantId' => $tenantId,
+        ]);
+    }
+}
---
+++ src/FusionAuth/README.md
@@ -0,0 +1,52 @@
+# FusionAuth
+
+```bash
+composer require socialiteproviders/fusionauth
+```
+
+## Installation & Basic Usage
+
+Please see the [Base Installation Guide](https://socialiteproviders.com/usage/), then follow the provider specific instructions below.
+
+### Add configuration to `config/services.php`
+
+```php
+'fusionauth' => [
+    'client_id' => env('FUSIONAUTH_CLIENT_ID'),
+    'client_secret' => env('FUSIONAUTH_CLIENT_SECRET'),
+    'redirect' => env('FUSIONAUTH_REDIRECT_URI'),
+    'base_url' => env('FUSIONAUTH_BASE_URL'), // Base URL of your cloud instance or self hosted instance
+    'tenant_id' => env('FUSIONAUTH_TENANT_ID'), // Tenant ID of the client. Required since FusionAuth 1.8.0
+],
+```
+
+### Add provider event listener
+
+Configure the package's listener to listen for `SocialiteWasCalled` events.
+
+Add the event to your `listen[]` array in `app/Providers/EventServiceProvider`. See the [Base Installation Guide](https://socialiteproviders.com/usage/) for detailed instructions.
+
+```php
+protected $listen = [
+    \SocialiteProviders\Manager\SocialiteWasCalled::class => [
+        // ... other providers
+        'SocialiteProviders\\FusionAuth\\FusionAuthExtendSocialite@handle',
+    ],
+];
+```
+
+### Usage
+
+You should now be able to use the provider like you would regularly use Socialite (assuming you have the facade installed):
+
+```php
+return Socialite::driver('fusionauth')->redirect();
+```
+
+### Returned User fields
+
+- ``id``
+- ``nickname``
+- ``name``
+- ``email``
+- ``avatar``
