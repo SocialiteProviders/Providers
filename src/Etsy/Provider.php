ca7a32258f03d18def8af330d267613cdadc7a47
atymic <atymicq@gmail.com>
2022-10-20 09:37:33 +1100

    chore: style fixes

--- src/Etsy/Provider.php
+++ src/Etsy/Provider.php
@@ -68,7 +68,7 @@ class Provider extends AbstractProvider
         return (new User())->setRaw($user)->map([
             'id'       => $user['user_id'],
             'nickname' => $user['first_name'],
-            'name'     => trim($user['first_name'] . ' ' . $user['last_name']),
+            'name'     => trim($user['first_name'].' '.$user['last_name']),
             'email'    => $user['primary_email'],
             'avatar'   => $user['image_url_75x75'],
         ]);
