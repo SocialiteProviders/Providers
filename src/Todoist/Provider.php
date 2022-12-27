99fafc413308679b1374e94a355bbb09942715e4
Omar Samuels <2964789+odsamuels@users.noreply.github.com>
2022-12-27 00:04:50 -0500

    Fixes Todoist API Token Auth implementation (#959)

--- src/Todoist/Provider.php
+++ src/Todoist/Provider.php
@@ -46,8 +46,10 @@ class Provider extends AbstractProvider
     protected function getUserByToken($token)
     {
         $response = $this->getHttpClient()->get(self::SYNC_URL, [
+            'headers' => [
+                'Authorization' => 'Bearer '.$token,
+            ],
             RequestOptions::QUERY => [
-                'token'          => $token,
                 'sync_token'     => '*',
                 'resource_types' => json_encode(['user']),
             ],
