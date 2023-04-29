79932f2c928b4fab166e5b647f5ea4ef709854ba
Elliot <1073323+kurucu@users.noreply.github.com>
2023-04-29 14:45:53 +0000

    Added default user object mapping attributes

--- src/AzureADB2C/Provider.php
+++ src/AzureADB2C/Provider.php
@@ -205,7 +205,10 @@ class Provider extends AbstractProvider
     protected function mapUserToObject(array $user)
     {
         return (new User())->setRaw($user)->map([
-            'id'   => $user['sub'],
+            'id' => $user['sub'],
+            'nickname' => $user['name'],
+            'name' => $user['name'],
+            'email' => $user['emails'][0],
         ]);
     }
 
