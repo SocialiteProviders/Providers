563482605713cd5b2b7f7e5be47ae57906820e87
Gustav Olsson <g@gustavolsson.se>
2023-05-05 09:47:59 +0200

    Rid unnecessary override of getTokenField($code)

--- src/Apple/Provider.php
+++ src/Apple/Provider.php
@@ -99,16 +99,6 @@ class Provider extends AbstractProvider
     /**
      * {@inheritdoc}
      */
-    protected function getTokenFields($code)
-    {
-        return array_merge(parent::getTokenFields($code), [
-            'grant_type' => 'authorization_code',
-        ]);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
     protected function getUserByToken($token)
     {
         static::verify($token);
