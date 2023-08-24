724d0174a7e8c437b1a2abfc1aebf78b86f35f88
Cosmin B <cosmin.badea.it@gmail.com>
2023-08-24 17:48:48 +0300

    Update GoogleExtendSocialite.php

--- src/Google/GoogleExtendSocialite.php
+++ src/Google/GoogleExtendSocialite.php
@@ -6,6 +6,11 @@ use SocialiteProviders\Manager\SocialiteWasCalled;
 
 class GoogleExtendSocialite
 {
+    /**
+     * Register the provider.
+     *
+     * @param \SocialiteProviders\Manager\SocialiteWasCalled $socialiteWasCalled
+     */
     public function handle(SocialiteWasCalled $socialiteWasCalled)
     {
         $socialiteWasCalled->extendSocialite('google', Provider::class);
