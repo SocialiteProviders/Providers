e7a971b9a5544c51805697b8efb585063469ff49
Lucas Michot <lucas@semalead.com>
2024-01-26 14:52:43 +0100

    Normalize the case of the identifiers names (#1093)

--- src/MakerLog/Provider.php
+++ src/MakerLog/Provider.php
@@ -8,7 +8,7 @@ use SocialiteProviders\Manager\OAuth2\User;
 
 class Provider extends AbstractProvider
 {
-    public const IDENTIFIER = 'makerlog';
+    public const IDENTIFIER = 'MAKERLOG';
 
     /**
      * {@inheritdoc}
