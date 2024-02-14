b4bebf895b57595af28ae3c791862662d4a82015
Lucas Michot <lucas@semalead.com>
2024-02-14 15:51:48 +0100

    Remove useless comments (#1149)

--- src/StartGg/StartGgExtendSocialite.php
+++ src/StartGg/StartGgExtendSocialite.php
@@ -6,11 +6,6 @@ use SocialiteProviders\Manager\SocialiteWasCalled;
 
 class StartGgExtendSocialite
 {
-    /**
-     * Register the provider.
-     *
-     * @param  \SocialiteProviders\Manager\SocialiteWasCalled  $socialiteWasCalled
-     */
     public function handle(SocialiteWasCalled $socialiteWasCalled)
     {
         $socialiteWasCalled->extendSocialite('startgg', Provider::class);
