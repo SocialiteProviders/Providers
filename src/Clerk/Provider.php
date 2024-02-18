8bb83777cf5d4fb69d99e5bc93c2509a13e216d8
Ignacio José Canó Cabral <ignaciojcano@gmail.com>
2024-02-18 14:03:20 -0500

    chore: remove unneeded method

--- src/Clerk/Provider.php
+++ src/Clerk/Provider.php
@@ -80,14 +80,4 @@ class Provider extends AbstractProvider
             'avatar'   => $user['picture'],
         ]);
     }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function getTokenFields($code)
-    {
-        return array_merge(parent::getTokenFields($code), [
-            'grant_type' => 'authorization_code'
-        ]);
-    }
 }
