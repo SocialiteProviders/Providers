d15b16fdff854fb7abd2227914eca29495924f80
Ignacio José Canó Cabral <ignacioj.cano@gmail.com>
2024-02-19 07:17:59 -0500

    feat: implement Clerk provider (#1153)
    Co-authored-by: Ignacio José Canó Cabral <ignaciojcano@gmail.com>
    Co-authored-by: Lucas Michot <lucas@semalead.com>

---
+++ src/Clerk/Provider.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace SocialiteProviders\Clerk;
+
+use GuzzleHttp\RequestOptions;
+use InvalidArgumentException;
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider
+{
+    public const IDENTIFIER = 'CLERK';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopes = ['email', 'profile'];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopeSeparator = ' ';
+
+    /**
+     * Get the base URL.
+     *
+     * @return string
+     *
+     * @throws \InvalidArgumentException
+     */
+    protected function getBaseUrl()
+    {
+        $baseUrl = $this->getConfig('base_url');
+
+        if ($baseUrl === null) {
+            throw new InvalidArgumentException('Missing Base URL value.');
+        }
+
+        return $baseUrl;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function additionalConfigKeys(): array
+    {
+        return ['base_url'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state): string
+    {
+        return $this->buildAuthUrlFromBase($this->getBaseUrl().'/oauth/authorize', $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl(): string
+    {
+        return $this->getBaseUrl().'/oauth/token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get($this->getBaseUrl().'/oauth/userinfo', [
+            RequestOptions::HEADERS => [
+                'Authorization' => 'Bearer '.$token,
+            ],
+        ]);
+
+        return json_decode((string) $response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User)->setRaw($user)->map([
+            'id' => $user['user_id'],
+            'nickname' => $user['username'],
+            'name' => $user['name'],
+            'email' => $user['email'],
+            'avatar' => $user['picture'],
+        ]);
+    }
+}
