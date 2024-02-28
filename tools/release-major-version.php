dd23b9edd6bf8bbf58b0f73b3f74836529a82011
atymic <atymicq@gmail.com>
2024-02-28 18:36:07 +0100

    fix: fix zttp deprecation, bump deps

--- tools/release-major-version.php
+++ tools/release-major-version.php
@@ -1,9 +1,11 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use Composer\Semver\Comparator;
-use Zttp\Zttp;
+use Illuminate\Http\Client\Factory;
+
+$http = new Factory();
 
 /**
  * Release a new major version across all providers.
@@ -20,17 +22,18 @@ $excludedRepos = [
 define('NEW_VERSION', '4.0.0');
 
 $repos = collect(range(1, 5))
-    ->map(fn (int $page) => Zttp::withHeaders(['Accept' => 'application/vnd.github.v3+json'])->get('https://api.github.com/orgs/SocialiteProviders/repos?per_page=100&page='.$page)->json())
+    ->map(fn(int $page
+    ) => $http->withHeaders(['Accept' => 'application/vnd.github.v3+json'])->get('https://api.github.com/orgs/SocialiteProviders/repos?per_page=100&page=' . $page)->json())
     ->flatten(1)
-    ->filter(fn (array $repo) => $repo['description'] && str_contains($repo['description'], '[READ ONLY] Subtree split'))
+    ->filter(fn(array $repo) => $repo['description'] && str_contains($repo['description'], '[READ ONLY] Subtree split'))
     ->sortBy('name')
-    ->each(function (array $repo) {
-        $res = Zttp::withHeaders([
-            'Accept'        => 'application/vnd.github.v3+json',
-            'Authorization' => 'token '.getenv('GITHUB_TOKEN'),
-        ])->get($repo['url'].'/releases');
+    ->each(function (array $repo) use ($http) {
+        $res = $http->withHeaders([
+            'Accept' => 'application/vnd.github.v3+json',
+            'Authorization' => 'token ' . getenv('GITHUB_TOKEN'),
+        ])->get($repo['url'] . '/releases');
 
-        $higherThanNew = collect($res->json())->filter(fn (array $rel) => Comparator::greaterThanOrEqualTo($rel['tag_name'], NEW_VERSION));
+        $higherThanNew = collect($res->json())->filter(fn(array $rel) => Comparator::greaterThanOrEqualTo($rel['tag_name'], NEW_VERSION));
 
         if ($higherThanNew->isNotEmpty()) {
             echo sprintf("Found Higher Release %s for provider %s, skipping\n", $higherThanNew->first()['tag_name'], $repo['name']);
@@ -38,14 +41,14 @@ $repos = collect(range(1, 5))
             return;
         }
 
-        $res = Zttp::withHeaders([
-            'Accept'        => 'application/vnd.github.v3+json',
-            'Authorization' => 'token '.getenv('GITHUB_TOKEN'),
-        ])->post($repo['url'].'/releases', [
-            'tag_name'         => NEW_VERSION,
+        $res = $http->withHeaders([
+            'Accept' => 'application/vnd.github.v3+json',
+            'Authorization' => 'token ' . getenv('GITHUB_TOKEN'),
+        ])->post($repo['url'] . '/releases', [
+            'tag_name' => NEW_VERSION,
             'target_commitish' => 'master',
-            'name'             => 'Release V4',
-            'body'             => "- Drop PHP < 7.2\n- Drop Laravel < 6",
+            'name' => 'Release V4',
+            'body' => "- Drop PHP < 7.2\n- Drop Laravel < 6",
         ]);
 
         echo sprintf("Released Version for Repo: %s, response code: %s\n", $repo['name'], $res->status());
