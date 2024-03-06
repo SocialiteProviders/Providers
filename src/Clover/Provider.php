cc426e154069279cc4a76b960475171c97c12b35
Andrew Minion <andrew@andrewrminion.com>
2024-03-06 14:25:58 -0600

    finish Clover (#1169)

--- src/Clover/Provider.php
+++ src/Clover/Provider.php
@@ -2,6 +2,8 @@
 
 namespace SocialiteProviders\Clover;
 
+use GuzzleHttp\RequestOptions;
+use Illuminate\Support\Arr;
 use SocialiteProviders\Manager\OAuth2\AbstractProvider;
 use SocialiteProviders\Manager\OAuth2\User;
 
@@ -24,25 +26,40 @@ class Provider extends AbstractProvider
      */
     protected $stateless = true;
 
-    public static function additionalConfigKeys()
+    public static function additionalConfigKeys(): array
     {
         return [
             'environment',
         ];
     }
 
+    /**
+     * Get the access token response for the given code.
+     *
+     * @param  string  $code
+     */
+    public function getAccessTokenResponse($code): array
+    {
+        $response = $this->getHttpClient()->post($this->getTokenUrl(), [
+            RequestOptions::HEADERS => $this->getTokenHeaders($code),
+            RequestOptions::JSON => $this->getTokenFields($code),
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
     protected function getApiDomain(): string
     {
         return match ($this->getConfig('environment')) {
             'sandbox' => 'sandbox.dev.clover.com',
-            default   => 'www.clover.com',
+            default => 'www.clover.com',
         };
     }
 
     /**
      * {@inheritdoc}
      */
-    protected function getAuthUrl($state)
+    protected function getAuthUrl($state): string
     {
         return $this->buildAuthUrlFromBase(
             sprintf('https://%s/oauth/v2/authorize', $this->getApiDomain()),
@@ -53,14 +70,14 @@ class Provider extends AbstractProvider
     /**
      * {@inheritdoc}
      */
-    protected function getTokenUrl()
+    protected function getTokenUrl(): string
     {
         $domain = match ($this->getConfig('environment')) {
             'sandbox' => 'apisandbox.dev.clover.com',
-            default   => 'api.clover.com',
+            default => 'api.clover.com',
         };
 
-        return sprintf('https://%s/oauth/token', $domain);
+        return sprintf('https://%s/oauth/v2/token', $domain);
     }
 
     /**
@@ -68,16 +85,11 @@ class Provider extends AbstractProvider
      */
     protected function getUserByToken($token)
     {
-        $requestParams = str(request()->fullUrl())
-            ->after('?')
-            ->explode('&')
-            ->mapWithKeys(fn (string $keyAndValue) => [str($keyAndValue)->before('=')->toString() => str($keyAndValue)->after('=')->toString()]);
-
         $response = $this->getHttpClient()->get(sprintf(
             'https://%s/v3/merchants/%s/employees/%s',
             $this->getApiDomain(),
-            $requestParams['merchant_id'],
-            $requestParams['employee_id'],
+            $this->request->query('merchant_id'),
+            $this->request->query('employee_id'),
         ), [
             'headers' => [
                 'Authorization' => 'Bearer '.$token,
@@ -93,11 +105,22 @@ class Provider extends AbstractProvider
     protected function mapUserToObject(array $user)
     {
         return (new User())->setRaw($user)->map([
-            'id'       => $user['id'],
+            'id' => $user['id'],
             'nickname' => $user['name'],
-            'name'     => $user['name'],
-            'email'    => $user['email'],
-            'avatar'   => null,
+            'name' => $user['name'],
+            'email' => $user['email'],
+            'avatar' => null,
         ]);
+    }
+
+    /**
+     * Get the expires in from the token response body.
+     *
+     * @param  array  $body
+     * @return string
+     */
+    protected function parseExpiresIn($body)
+    {
+        return (string) (Arr::get($body, 'access_token_expiration') - time());
     }
 }
