39b53d428df888aff6ebbb7a07c4ead37581f58b
Olivier <47295195+sympact06@users.noreply.github.com>
2024-04-12 17:21:26 +0200

    Add Roblox OAuth2 Provider for Laravel Socialite
    This commit introduces the Roblox OAuth2 Provider for Laravel Socialite. It includes the provider's installation and usage instructions in the README file as well as the implementation of the Provider class. It also updates the monorepo-builder.yml file to include a new reference to the Roblox provider repository.

---
+++ src/Roblox/Provider.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace SocialiteProviders\Roblox;
+
+use GuzzleHttp\RequestOptions;
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider
+{
+    public const IDENTIFIER = 'ROBLOX';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopes = ['openid', 'profile'];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopeSeparator = ' ';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase(
+            'https://apis.roblox.com/oauth/v1/authorize',
+            $state
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getCodeFields($state = null)
+    {
+        return array_merge(parent::getCodeFields($state), [
+            'client_id' => $this->clientId,
+            'response_type' => 'code',
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://apis.roblox.com/oauth/v1/token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get(
+            'https://apis.roblox.com/oauth/v1/userinfo',
+            [
+                RequestOptions::HEADERS => [
+                    'Authorization' => 'Bearer ' . $token,
+                ],
+            ]
+        );
+
+        return json_decode((string) $response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id'       => $user['sub'],  // Roblox user ID
+            'username' => $user['preferred_username'], // Roblox username (not display name)
+            'nickname' => $user['preferred_username'], // Roblox display name (not guaranteed to be unique)
+            'picture'   => $user['picture'] ?? null,  // Roblox may leave this null if the account is new
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function additionalConfigKeys()
+    {
+        return [];
+    }
+}
