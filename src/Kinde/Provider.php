d99917e33ce5fbafc1ae2fe4b8151b51b2aa9941
Mikko Kutilainen <mikko93@gmail.com>
2024-11-29 18:24:12 +0200

    WIP

---
+++ src/Kinde/Provider.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace SocialiteProviders\Kinde;
+
+use GuzzleHttp\RequestOptions;
+use InvalidArgumentException;
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider
+{
+    public const IDENTIFIER = 'KINDE';
+
+    protected $scopes = ['address', 'email', 'offline', 'openid', 'phone', 'profile'];
+
+    protected $scopeSeparator = ' ';
+
+    /**
+     * Get the domain (aka base URL)
+     *
+     * @return string
+     *
+     * @throws \InvalidArgumentException
+     */
+    protected function getDomain()
+    {
+        $baseUrl = $this->getConfig('domain');
+
+        if ($baseUrl === null) {
+            throw new InvalidArgumentException('Missing Base URL value.');
+        }
+
+        return $baseUrl;
+    }
+
+    public static function additionalConfigKeys(): array
+    {
+        return ['domain'];
+    }
+
+    protected function getAuthUrl($state): string
+    {
+        return $this->buildAuthUrlFromBase($this->getDomain().'/oauth2/auth', $state);
+    }
+
+    protected function getTokenUrl(): string
+    {
+        return $this->getDomain().'/oauth2/token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get($this->getDomain().'/oauth2/user_profile', [
+            RequestOptions::HEADERS => [
+                'Authorization' => 'Bearer '.$token,
+            ],
+        ]);
+
+        $user = json_decode((string) $response->getBody(), true);
+
+        // Merge user details with org_code and permissions from the token
+        // No need to validate the token because it is already validated
+        // by succesful profile request.
+
+        $payload = json_decode(base64_decode(explode('.', $token)[1]), true); // assuming valid JWT token
+
+        return array_merge($user, [
+            'org_code'    => $payload['org_code'] ?? null,
+            'permissions' => $payload['permissions'] ?? [],
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User)->setRaw($user)->map([
+            'id'       => $user['id'],
+            'avatar'   => $user['picture'],
+            'name'     => $user['first_name'].' '.$user['last_name'],
+            'email'    => $user['preferred_email'],
+        ]);
+    }
+}
