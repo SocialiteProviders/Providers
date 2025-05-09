8c5a244f18601a5cb5fd58ef478d593eb25a2edf
Gabriel R. Barbosa <gabrieltads2016@gmail.com>
2025-05-09 00:00:22 -0300

    Create TikTokShopProvider.php

---
+++ TikTokShop/TikTokShopProvider.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Providers\Socialite\TikTokShop;
+
+use Laravel\Socialite\Two\AbstractProvider;
+use Laravel\Socialite\Two\ProviderInterface;
+use Laravel\Socialite\Two\User;
+
+class TikTokShopProvider extends AbstractProvider implements ProviderInterface
+{
+    /**
+     * TikTok Shop doesn’t use scopes at this step.
+     */
+    protected $scopes = [];
+
+    /**
+     * Build the authorization URL.
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase(
+            'https://auth.tiktok-shops.com/api/v2/oauth/authorize',
+            $state
+        );
+    }
+
+    /**
+     * Endpoint to exchange code for tokens.
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://auth.tiktok-shops.com/api/v2/token/get';
+    }
+
+    /**
+     * Override user() so we can map the shop data directly from the token payload.
+     */
+    public function user()
+    {
+        // 1) Exchange auth code for token data
+        $response = $this->getHttpClient()->get($this->getTokenUrl(), [
+            'query' => $this->getTokenFields($this->request->get('code')),
+        ]);
+
+        $json      = json_decode((string) $response->getBody(), true);
+        $tokenData = $json['data'] ?? $json;
+
+        // 2) Map to Socialite User
+        $user = (new User)->setRaw($tokenData)->map([
+            'id'       => $tokenData['open_id']        ?? null,
+            'nickname' => $tokenData['seller_name']    ?? null,
+            'name'     => $tokenData['seller_name']    ?? null,
+            'email'    => null,
+            'avatar'   => null,
+        ]);
+
+        // 3) Attach tokens
+        $user->token        = $tokenData['access_token']             ?? null;
+        $user->refreshToken = $tokenData['refresh_token']            ?? null;
+        $user->expiresIn    = $tokenData['access_token_expire_in']   ?? null;
+
+        return $user;
+    }
+
+    /**
+     * Build the query parameters for the token request.
+     */
+    protected function getTokenFields($code)
+    {
+        return [
+            'app_key'     => $this->clientId,
+            'app_secret'  => $this->clientSecret,
+            'auth_code'   => $code,
+            'grant_type'  => 'authorized_code',
+        ];
+    }
+}
