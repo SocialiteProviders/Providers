df7d96dccea81f9e54045f509bb69920d16d7f67
Winfred van Kuijk <winfred@vankuijk.net>
2025-12-28 15:36:11 +0100

    feat: added provider Tailscale

---
+++ src/Tailscale/TailscaleExtendSocialite.php
@@ -0,0 +1,14 @@
+
+<?php
+
+namespace SocialiteProviders\Tailscale;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class TailscaleExtendSocialite
+{
+    public function handle(SocialiteWasCalled $socialiteWasCalled): void
+    {
+        $socialiteWasCalled->extendSocialite('tailscale', Provider::class);
+    }
+}
